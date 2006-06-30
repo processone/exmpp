bb36210b85cfd3a3194a3c2a7f3c4aa61f0e8ba5
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2006-06-30 13:11:48 +0000

    Testsuite is almost complete and will not be extended to the deprecated API. Now, 83.5% of exmpp_xml is covered.
    SVN Revision: 19

--- testsuite/check_xml_converters.erl
+++ testsuite/check_xml_converters.erl
@@ -1,6 +1,6 @@
 % $Id$
 
--module(check_xml_serializer).
+-module(check_xml_converters).
 -vsn('$Revision$').
 
 -include("exmpp.hrl").
@@ -12,6 +12,7 @@ check() ->
 	testsuite:pass().
 
 do_check() ->
+	test_xmlnselement_to_xmlelement(),
 	test_document_to_list_without_namespace(),
 	test_document_to_list_with_namespace(),
 	test_document_fragment_to_list_with_namespace(),
@@ -21,6 +22,20 @@ do_check() ->
 % --------------------------------------------------------------------
 % Serliazer testsuite.
 % --------------------------------------------------------------------
+
+-define(XML_NS, 'http://www.w3.org/XML/1998/namespace').
+
+-define(TREE1_NO_NS,
+{xmlelement, "stream:stream", [
+	{"xmlns:stream", "ns_stream"}
+], []}
+).
+
+-define(TREE1_NS,
+{xmlnselement, 'ns_stream', "stream", "stream", [], []}
+).
+
+-define(SOURCE1, "<stream:stream xmlns:stream=\"ns_stream\"/>").
 
 -define(TREE2_NO_NS,
 {xmlelement, "stream:stream", [
@@ -38,8 +53,17 @@ do_check() ->
 -define(TREE2_NS,
 {xmlnselement, 'ns_stream', "stream", "stream", [], [
 	{xmlnselement, 'ns_default', undefined, "iq", [
-		{xmlattr, 'http://www.w3.org/XML/1998/namespace', undefined,
-		    "lang", "fr"}
+		{xmlattr, ?XML_NS, undefined, "lang", "fr"}
+	], [
+		{xmlcdata, <<"Content">>}
+	]}
+]}
+).
+
+-define(TREE2_NS_NAS,
+{xmlnselement, 'ns_stream', "stream", 'stream', [], [
+	{xmlnselement, 'ns_default', undefined, 'iq', [
+		{xmlattr, ?XML_NS, undefined, 'lang', "fr"}
 	], [
 		{xmlcdata, <<"Content">>}
 	]}
@@ -50,10 +74,11 @@ do_check() ->
 
 -define(TREE3_NS,
 {xmlnselement, 'ns_iq', undefined, "iq", [
-	{xmlattr, 'http://www.w3.org/XML/1998/namespace', undefined,
-	    "lang", "fr"}
+	{xmlattr, ?XML_NS, undefined, "lang", "fr"}
 ], [
-	{xmlcdata, <<"Content">>}
+	{xmlcdata, <<"Binary">>},
+	{xmlcdata, "List"},
+	{xmlcdata, <<"& < > \" '">>}
 ]}
 ).
 
@@ -64,7 +89,7 @@ do_check() ->
 -define(TREE3_PREFIXED_NS, [
 ]).
 
--define(SOURCE3, "<iq xml:lang=\"fr\">Content</iq>").
+-define(SOURCE3, "<iq xml:lang=\"fr\">BinaryList&amp; &lt; &gt; &quot; &apos;</iq>").
 
 -define(TREE3_DEFAULT_NS_2, [
 ]).
@@ -73,26 +98,66 @@ do_check() ->
 	{'ns_iq', "jabber"}
 ]).
 
--define(SOURCE3_2, "<jabber:iq xml:lang=\"fr\">Content</jabber:iq>").
+-define(SOURCE3_2, "<jabber:iq xml:lang=\"fr\">BinaryList&amp; &lt; &gt; &quot; &apos;</jabber:iq>").
+
+-define(TREE4_NO_NS,
+{xmlelement, "stream", [
+	{"xmlns:pfx", "ns_attr"},
+	{"pfx:foo", "bar"}
+], []}
+).
+
+-define(TREE4_NS,
+{xmlnselement, undefined, undefined, "stream", [
+	{xmlattr, 'ns_attr', "pfx", "foo", "bar"}
+], []}
+).
+
+test_xmlnselement_to_xmlelement() ->
+	testsuite:is(exmpp_xml:xmlnselement_to_xmlelement(?TREE1_NO_NS),
+	    ?TREE1_NO_NS),
+	testsuite:is(exmpp_xml:xmlnselement_to_xmlelement(?TREE1_NS),
+	    ?TREE1_NO_NS),
+	testsuite:is(exmpp_xml:xmlnselement_to_xmlelement(?TREE2_NO_NS),
+	    ?TREE2_NO_NS),
+	testsuite:is(exmpp_xml:xmlnselement_to_xmlelement(?TREE2_NS),
+	    ?TREE2_NO_NS),
+	testsuite:is(exmpp_xml:xmlnselement_to_xmlelement(?TREE2_NS_NAS),
+	    ?TREE2_NO_NS),
+	testsuite:is(exmpp_xml:xmlnselement_to_xmlelement(?TREE4_NO_NS),
+	    ?TREE4_NO_NS),
+	testsuite:is(exmpp_xml:xmlnselement_to_xmlelement(?TREE4_NS),
+	    ?TREE4_NO_NS),
+	ok.
 
 test_document_to_list_without_namespace() ->
 	testsuite:is(
+	    lists:flatten(exmpp_xml:document_to_list(?TREE1_NO_NS)),
+	    ?SOURCE1),
+	testsuite:is(
 	    lists:flatten(exmpp_xml:document_to_list(?TREE2_NO_NS)),
-	    ?SOURCE2).
+	    ?SOURCE2),
+	ok.
 
 test_document_to_list_with_namespace() ->
 	testsuite:is(
+	    lists:flatten(exmpp_xml:document_to_list(?TREE1_NS)),
+	    ?SOURCE1),
+	testsuite:is(
 	    lists:flatten(exmpp_xml:document_to_list(?TREE2_NS)),
-	    ?SOURCE2).
+	    ?SOURCE2),
+	ok.
 
 test_document_fragment_to_list_with_namespace() ->
 	testsuite:is(
 	    lists:flatten(exmpp_xml:document_fragment_to_list(?TREE3_NS,
 	    ?TREE3_DEFAULT_NS, ?TREE3_PREFIXED_NS)),
-	    ?SOURCE3).
+	    ?SOURCE3),
+	ok.
 
 test_document_fragment_to_list_with_namespace2() ->
 	testsuite:is(
 	    lists:flatten(exmpp_xml:document_fragment_to_list(?TREE3_NS,
 	    ?TREE3_DEFAULT_NS_2, ?TREE3_PREFIXED_NS_2)),
-	    ?SOURCE3_2).
+	    ?SOURCE3_2),
+	ok.
