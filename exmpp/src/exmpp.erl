845b4e64c9c8ecb659e8d74ccd9cfa8ef25e0663
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2007-02-15 16:01:11 +0000

    This module uses the `application' behaviour now. It starts `exmpp_sup'.
    SVN Revision: 177

--- exmpp/src/exmpp.erl
+++ exmpp/src/exmpp.erl
@@ -3,8 +3,9 @@
 %% @author Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
 
 %% @doc
-%% The module <strong>{@module}</strong> currently provide some generic
-%% utilities for other modules of this application.
+%% The module <strong>{@module}</strong> provides callbacks for
+%% application(3) as well as some generic utilities for other modules of
+%% this application.
 %%
 %% <p>
 %% It's not intended to be used directly.
@@ -13,11 +14,28 @@
 -module(exmpp).
 -vsn('$Revision$').
 
--export([version/0]).
+-behaviour(application).
+
+-export([
+  start/0,
+  version/0
+]).
+
+% application(3erl) callbacks.
+-export([
+  start/2,
+  stop/1,
+  config_change/3
+]).
 
 % --------------------------------------------------------------------
 % Generic utilities.
 % --------------------------------------------------------------------
+
+%% @spec () -> ok
+%% @doc Start applications which exmpp depends on then start exmpp.
+start() ->
+    application:start(exmpp).
 
 %% @spec () -> Version
 %%     Version = string()
@@ -25,3 +43,24 @@
 version() ->
 	{ok, Version} = application:get_key(exmpp, vsn),
 	Version.
+
+% --------------------------------------------------------------------
+% application(3erl) callbacks.
+% --------------------------------------------------------------------
+
+%% @hidden
+start(_Start_Type, _Start_Args) ->
+    exmpp_sup:start_link().
+
+%% @hidden
+stop(_State) ->
+    ok.
+
+%% @hidden
+config_change(Changed, New, Removed) ->
+    error_logger:info_msg("Config change:~n"
+      "Changed: ~p~n"
+      "New: ~p~n"
+      "Removed: ~p~n",
+      [Changed, New, Removed]),
+    ok.
