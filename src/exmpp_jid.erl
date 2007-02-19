f193dca08e2a303b8b8b77ea63aba29d663ef15a
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2007-02-19 16:58:46 +0000

    - Reworked JID parsing to handle more error cases. - Now, the resource for a bare JID is set to `undefined', not the empty string.
    SVN Revision: 181

--- src/exmpp_jid.erl
+++ src/exmpp_jid.erl
@@ -34,6 +34,20 @@
 % JID creation & conversion.
 % --------------------------------------------------------------------
 
+make_bare_jid(undefined, Server) ->
+    case exmpp_stringprep:nameprep(Server) of
+        error ->
+            {error, bad_server};
+        LServer ->
+            #jid{
+              user = undefined,
+              server = Server,
+              resource = undefined,
+              luser = undefined,
+              lserver = LServer,
+              lresource = undefined
+            }
+    end;
 make_bare_jid(User, Server) ->
     case exmpp_stringprep:nodeprep(User) of
         error ->
@@ -46,10 +60,10 @@ make_bare_jid(User, Server) ->
                     #jid{
                       user = User,
                       server = Server,
-                      resource = "",
+                      resource = undefined,
                       luser = LUser,
                       lserver = LServer,
-                      lresource = ""
+                      lresource = undefined
                     }
             end
     end.
@@ -64,10 +78,12 @@ make_jid(User, Server, Resource) ->
 
 jid_to_bare_jid(Jid) ->
     Jid#jid{
-      resource = "",
-      lresource = ""
+      resource = undefined,
+      lresource = undefined
     }.
 
+bare_jid_to_jid(Jid, undefined) ->
+    Jid;
 bare_jid_to_jid(Jid, Resource) ->
     case exmpp_stringprep:resourceprep(Resource) of
         error ->
@@ -84,47 +100,69 @@ bare_jid_to_jid(Jid, Resource) ->
 % --------------------------------------------------------------------
 
 string_to_jid(String) ->
-    string_to_jid1(String, "").
+    parse_jid(full, String, "").
 
 string_to_bare_jid(String) ->
-    case string_to_jid(String) of
-        {error, Reason} ->
-            {error, Reason};
-        Jid ->
-            jid_to_bare_jid(Jid)
-    end.
+    parse_jid(bare, String, "").
 
-% Parse user/node.
-string_to_jid1([$@ | _Jid_S], "") ->
-    {error, node_or_user_expected};
-string_to_jid1([$@ | Jid_S], User) ->
-    string_to_jid2(Jid_S, lists:reverse(User), "");
-string_to_jid1([$/ | _Jid_S], "") ->
-    {error, server_expected};
-string_to_jid1([$/ | Jid_S], User) ->
-    string_to_jid3(Jid_S, "", lists:reverse(User), "");
-string_to_jid1([C  | Jid_S], User) ->
-    string_to_jid1(Jid_S, [C | User]);
-string_to_jid1([], User) ->
-    make_jid("", lists:reverse(User), "").
+parse_jid(_Type, [$@ | _Rest], "") ->
+    % Invalid JID of the form "@Domain".
+    {error, unexpected_node_separator};
+parse_jid(Type, [$@ | Rest], Node) ->
+    % JID of the form "Node@Domain".
+    parse_jid(Type, Rest, lists:reverse(Node), "");
+parse_jid(_Type, [$/ | _Rest], "") ->
+    % Invalid JID of the form "/Resource".
+    {error, unexpected_resource_separator};
+parse_jid(full, [$/], _Domain) ->
+    % Invalid JID of the form "Domain/".
+    {error, unexpected_end_of_string};
+parse_jid(bare, [$/ | _Rest], Domain) ->
+    % Valid JID of the form "Domain/Resource" (resource is dropped).
+    make_bare_jid(undefined, lists:reverse(Domain));
+parse_jid(full, [$/ | Rest], Domain) ->
+    % Valid JID of the form "Domain/Resource".
+    make_jid(undefined, lists:reverse(Domain), Rest);
+parse_jid(Type, [C | Rest], Node_Or_Domain) ->
+    % JID of the form "Node@Domain" or "Node@Domain/Resource".
+    parse_jid(Type, Rest, [C | Node_Or_Domain]);
+parse_jid(_Type, [], "") ->
+    % Invalid JID of the form "".
+    {error, unexpected_end_of_string};
+parse_jid(bare, [], Domain) ->
+    % Valid JID of the form "Domain".
+    make_bare_jid(undefined, lists:reverse(Domain));
+parse_jid(full, [], Domain) ->
+    % Valid JID of the form "Domain".
+    make_jid(undefined, lists:reverse(Domain), undefined).
 
-% Parser server.
-string_to_jid2([$@ | _Jid_S], _User, _Server) ->
-    {error, at_character_not_allowed};
-string_to_jid2([$/ | _Jid_S], _User, "") ->
-    {error, server_expected};
-string_to_jid2([$/ | Jid_S], User, Server) ->
-    string_to_jid3(Jid_S, User, lists:reverse(Server), "");
-string_to_jid2([C  | Jid_S], User, Server) ->
-    string_to_jid2(Jid_S, User, [C | Server]);
-string_to_jid2([], User, Server) ->
-    make_jid(User, lists:reverse(Server), "").
-
-% Parse resource.
-string_to_jid3([C  | Jid_S], User, Server, Res) ->
-    string_to_jid3(Jid_S, User, Server, [C | Res]);
-string_to_jid3([], User, Server, Res) ->
-    make_jid(User, Server, lists:reverse(Res)).
+parse_jid(_Type, [$@ | _Rest], _Node, _Domain) ->
+    % Invalid JID of the form "Node@Domain@Domain".
+    {error, unexpected_node_separator};
+parse_jid(_Type, [$/ | _Rest], _Node, "") ->
+    % Invalid JID of the form "Node@/Resource".
+    {error, unexpected_resource_separator};
+parse_jid(full, [$/], _Node, _Domain) ->
+    % Invalid JID of the form "Node@Domain/".
+    {error, unexpected_end_of_string};
+parse_jid(bare, [$/ | _Rest], Node, Domain) ->
+    % Valid JID of the form "Node@Domain/Resource" (resource is dropped).
+    make_bare_jid(Node, lists:reverse(Domain));
+parse_jid(full, [$/ | Rest], Node, Domain) ->
+    % Valid JID of the form "Node@Domain/Resource".
+    make_jid(Node, lists:reverse(Domain), Rest);
+parse_jid(Type, [C | Rest], Node, Domain) ->
+    % JID of the form "Node@Domain" or "Node@Domain/Resource".
+    parse_jid(Type, Rest, Node, [C | Domain]);
+parse_jid(_Type, [], _Node, "") ->
+    % Invalid JID of the form "Node@".
+    {error, unexpected_end_of_string};
+parse_jid(bare, [], Node, Domain) ->
+    % Valid JID of the form "Node@Domain".
+    make_bare_jid(Node, lists:reverse(Domain));
+parse_jid(full, [], Node, Domain) ->
+    % Valid JID of the form "Node@Domain".
+    make_jid(Node, lists:reverse(Domain), undefined).
 
 % --------------------------------------------------------------------
 % JID serialization.
@@ -136,8 +174,8 @@ jid_to_string(#jid{user = User, server = Server, resource = Res}) ->
 jid_to_string(User, Server, Res) ->
     S1 = bare_jid_to_string(User, Server),
     case Res of
-        "" -> S1;
-        _  -> S1 ++ "/" ++ Res
+        undefined -> S1;
+        _         -> S1 ++ "/" ++ Res
     end.
 
 bare_jid_to_string(#jid{user = User, server = Server}) ->
@@ -145,7 +183,7 @@ bare_jid_to_string(#jid{user = User, server = Server}) ->
 
 bare_jid_to_string(User, Server) ->
     S1 = case User of
-        "" -> "";
-        _  -> User ++ "@"
+        undefined -> "";
+        _         -> User ++ "@"
     end,
     S1 ++ Server.
