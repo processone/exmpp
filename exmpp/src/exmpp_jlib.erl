2591e89c031cd6069221345f126f6705e369c403
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2007-02-20 16:15:34 +0000

    Drop JID handling functions.
    SVN Revision: 186

--- exmpp/src/exmpp_jlib.erl
+++ exmpp/src/exmpp_jlib.erl
@@ -14,12 +14,6 @@
   rename_attr_to_to_from/1
 ]).
 
--export([
-  jid_to_string/1,
-  string_to_jid/1,
-  make_jid/3
-]).
-
 % --------------------------------------------------------------------
 % Helpers to handle common attribute operations.
 % --------------------------------------------------------------------
@@ -80,70 +74,3 @@ rename_attr_to_to_from(#xmlnselement{attrs = Attrs} = XML_Element) ->
 rename_attr_to_to_from(#xmlelement{attrs = Attrs} = XML_Element) ->
     New_Attrs = rename_attr_to_to_from_in_list(Attrs),
     XML_Element#xmlelement{attrs = New_Attrs}.
-
-% --------------------------------------------------------------------
-% JID handling.
-% --------------------------------------------------------------------
-
-jid_to_string(#jid{user = User, server = Server, resource = Res}) ->
-    jid_to_string(User, Server, Res).
-
-jid_to_string(Node, Server, Res) ->
-    S1 = case Node of
-        "" -> "";
-        _  -> Node ++ "@"
-    end,
-    S2 = S1 ++ Server,
-    S3 = case Res of
-        "" -> S2;
-        _  -> S2 ++ "/" ++ Res
-    end,
-    S3.
-
-string_to_jid(Jid_S) ->
-    string_to_jid1(Jid_S, "").
-
-% Parse user/node.
-string_to_jid1([$@ | _Jid_S], "") ->
-    {error, node_or_user_expected};
-string_to_jid1([$@ | Jid_S], Node) ->
-    string_to_jid2(Jid_S, lists:reverse(Node), "");
-string_to_jid1([$/ | _Jid_S], "") ->
-    {error, server_expected};
-string_to_jid1([$/ | Jid_S], Node) ->
-    string_to_jid3(Jid_S, "", lists:reverse(Node), "");
-string_to_jid1([C  | Jid_S], Node) ->
-    string_to_jid1(Jid_S, [C | Node]);
-string_to_jid1([], Node) ->
-    make_jid("", lists:reverse(Node), "").
-
-% Parser server.
-string_to_jid2([$@ | _Jid_S], _Node, _Server) ->
-    {error, at_character_not_allowed};
-string_to_jid2([$/ | _Jid_S], _Node, "") ->
-    {error, server_expected};
-string_to_jid2([$/ | Jid_S], Node, Server) ->
-    string_to_jid3(Jid_S, Node, lists:reverse(Server), "");
-string_to_jid2([C  | Jid_S], Node, Server) ->
-    string_to_jid2(Jid_S, Node, [C | Server]);
-string_to_jid2([], Node, Server) ->
-    make_jid(Node, lists:reverse(Server), "").
-
-% Parse resource.
-string_to_jid3([C  | Jid_S], Node, Server, Res) ->
-    string_to_jid3(Jid_S, Node, Server, [C | Res]);
-string_to_jid3([], Node, Server, Res) ->
-    make_jid(Node, Server, lists:reverse(Res)).
-
-% Make #jid{}.
-% XXX The original function uses `stringprep'. This one should do the same
-% but `stringprep' must be renamed/reworked to become, eg, `exmpp_stringprep'.
-make_jid(Node, Server, Resource) ->
-    #jid{
-      user = Node,
-      server = Server,
-      resource = Resource,
-      luser = Node,
-      lserver = Server,
-      lresource = Resource
-    }.
