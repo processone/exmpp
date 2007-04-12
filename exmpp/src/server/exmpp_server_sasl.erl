2e2813eb0992e83204844bf695bcc559c3c96df2
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2007-04-12 14:03:05 +0000

    New modules to deal with SASL transport.
    SVN Revision: 229

---
+++ exmpp/src/server/exmpp_server_sasl.erl
@@ -0,0 +1,86 @@
+% $Id$
+
+%% @author Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
+
+-module(exmpp_server_sasl).
+-vsn('$Revision$').
+
+-include("exmpp.hrl").
+
+-export([
+  feature/1,
+  challenge/1,
+  failure/0,
+  failure/1,
+  next_step/1
+]).
+
+% --------------------------------------------------------------------
+% Feature announcement.
+% --------------------------------------------------------------------
+
+feature(Mechanisms) ->
+    case mechanisms_list(Mechanisms) of
+        {error, Reason} ->
+            {error, Reason};
+        Children ->
+            #xmlnselement{
+              ns = ?NS_SASL,
+              name = 'mechanisms',
+              children = Children
+            }
+    end.
+
+mechanisms_list(Mechanisms) ->
+    mechanisms_list2(Mechanisms, []).
+
+mechanisms_list2([Mechanism | Rest], Children) ->
+    Child = #xmlnselement{
+      ns = ?NS_SASL,
+      name = 'mechanism',
+      children = []
+    },
+    mechanisms_list2(Rest,
+      Children ++ [exmpp_xml:set_cdata(Child, Mechanism)]);
+mechanisms_list2([], Children) ->
+    Children.
+
+% --------------------------------------------------------------------
+% SASL exchange.
+% --------------------------------------------------------------------
+
+challenge(Challenge) ->
+    El = #xmlnselement{
+      ns = ?NS_SASL,
+      name = 'challenge',
+      children = []
+    },
+    exmpp_xml:set_cdata(El, http_base_64:encode(Challenge)).
+
+failure() ->
+    #xmlnselement{
+      ns = ?NS_SASL,
+      name = 'failure',
+      children = []
+    }.
+
+failure(Reason) ->
+    El = #xmlnselement{
+      ns = ?NS_SASL,
+      name = list_to_atom(Reason),
+      children = []
+    },
+    exmpp_xml:append_child(failure(), El).
+
+next_step(#xmlnselement{ns = ?NS_SASL, name = 'auth'} = El) ->
+    Mechanism = exmpp_xml:get_attribute(El, 'mechanism'),
+    case string:strip(exmpp_xml:get_cdata(El)) of
+        ""      -> {auth, Mechanism};
+        "="     -> {auth, Mechanism, ""};
+        Encoded -> {auth, Mechanism, http_base_64:decode(Encoded)}
+    end;
+next_step(#xmlnselement{ns = ?NS_SASL, name = 'response'} = El) ->
+    Encoded = exmpp_xml:get_cdata(El),
+    {response, http_base_64:decode(Encoded)};
+next_step(#xmlnselement{ns = ?NS_SASL, name = 'abort'}) ->
+    abort.
