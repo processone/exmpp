7900d6fd7414753784ced2a0bfe11a13e843484b
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2007-06-19 09:31:43 +0000

    Add helpers to access common attributes.
    SVN Revision: 246

--- exmpp/src/client/exmpp_client_stream.erl
+++ exmpp/src/client/exmpp_client_stream.erl
@@ -8,7 +8,10 @@
 -export([
   opening/1,
   closing/0,
-  closing/1
+  closing/1,
+  get_domain/1,
+  get_version/1,
+  get_lang/1
 ]).
 
 % --------------------------------------------------------------------
@@ -23,3 +26,20 @@ closing() ->
 
 closing(Opening) ->
     exmpp_stream:closing(Opening).
+
+% --------------------------------------------------------------------
+% Stream standard attributes.
+% --------------------------------------------------------------------
+
+get_domain(Opening) ->
+    case exmpp_xml:get_attribute(Opening, 'to') of
+        false -> false;
+        ""    -> false;
+        To    -> To
+    end.
+
+get_version(Opening) ->
+    exmpp_stream:get_version(Opening).
+
+get_lang(Opening) ->
+    exmpp_stream:get_lang(Opening).
--- exmpp/src/server/exmpp_server_stream.erl
+++ exmpp/src/server/exmpp_server_stream.erl
@@ -12,6 +12,10 @@
   opening_reply/1,
   closing/0,
   closing/1,
+  get_domain/1,
+  get_version/1,
+  get_id/1,
+  get_lang/1,
   features/1
 ]).
 
@@ -32,12 +36,36 @@ closing(Opening) ->
     exmpp_stream:closing(Opening).
 
 % --------------------------------------------------------------------
+% Stream standard attributes.
+% --------------------------------------------------------------------
+
+get_domain(Opening) ->
+    case exmpp_xml:get_attribute(Opening, 'from') of
+        false -> false;
+        ""    -> false;
+        From  -> From
+    end.
+
+get_version(Opening) ->
+    exmpp_stream:get_version(Opening).
+
+get_id(Opening) ->
+    case exmpp_xml:get_attribute(Opening, 'id') of
+        false -> false;
+        Id    -> Id
+    end.
+
+get_lang(Opening) ->
+    exmpp_stream:get_lang(Opening).
+
+% --------------------------------------------------------------------
 % Features announcement.
 % --------------------------------------------------------------------
 
 features(Features) ->
     #xmlnselement{
       ns = ?NS_XMPP,
+      prefix = "stream",
       name = 'features',
       children = Features
     }.
