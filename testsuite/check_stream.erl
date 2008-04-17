b8d60f7b986bfc68e27eaa765375069f5a121fdb
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-04-17 14:12:37 +0000

    o  Now, everything at the stream-level is handled here. exmpp_client_stream and exmpp_server_stream should be deleted as soon as the functions to manipulate features have moved here. API of existing functions has changed. o  Use exceptions for error handling. o  Update testsuite.
    SVN Revision: 338

--- testsuite/check_stream.erl
+++ testsuite/check_stream.erl
@@ -8,156 +8,137 @@
 -export([check/0, do_check/0]).
 
 check() ->
-	do_check(),
-	testsuite:pass().
+    do_check(),
+    testsuite:pass().
 
 do_check() ->
-	test_stream_opening(),
-	test_stream_opening_reply(),
-	test_stream_closing(),
-	ok.
+    test_stream_opening(),
+    test_stream_opening_reply(),
+    test_stream_closing(),
+    ok.
 
 % --------------------------------------------------------------------
 % Stream handling testsuite.
 % --------------------------------------------------------------------
 
 -define(OPENING1,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:client"}
-], undefined}
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'to', "dest"}
+    ], undefined}
 ).
 
 -define(OPENING2,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:server"}
-], undefined}
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'to', "dest"},
+      {xmlattr, undefined, undefined, 'version', "1.0"}
+    ], undefined}
 ).
 
 -define(OPENING3,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:client"},
-	{xmlattr, undefined, undefined, 'to', "dest"}
-], undefined}
-).
-
--define(OPENING4,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:client"},
-	{xmlattr, ?NS_XML, undefined, 'lang', "fr"}
-], undefined}
-).
-
--define(OPENING5,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:client"},
-	{xmlattr, undefined, undefined, 'version', "1.0"}
-], undefined}
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'to', "dest"},
+      {xmlattr, undefined, undefined, 'version', "1.0"},
+      {xmlattr, ?NS_XML, undefined, 'lang', "fr"}
+    ], undefined}
 ).
 
 -define(OPENING_REPLY1,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:client"},
-	{xmlattr, undefined, undefined, 'id', "foobar"}
-], undefined}
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'from', "dest"},
+      {xmlattr, undefined, undefined, 'id', "foobar"}
+    ], undefined}
 ).
 
 -define(OPENING_REPLY2,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:server"},
-	{xmlattr, undefined, undefined, 'id', "foobar"}
-], undefined}
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'from', "dest"},
+      {xmlattr, undefined, undefined, 'version', "1.0"},
+      {xmlattr, undefined, undefined, 'id', "foobar"}
+    ], undefined}
 ).
 
 -define(OPENING_REPLY3,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:client"},
-	{xmlattr, undefined, undefined, 'id', "foobar"},
-	{xmlattr, undefined, undefined, 'from', "orig"}
-], undefined}
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'from', "dest"},
+      {xmlattr, undefined, undefined, 'version', "1.0"},
+      {xmlattr, undefined, undefined, 'id', "foobar"},
+      {xmlattr, ?NS_XML, undefined, 'lang', "fr"}
+    ], undefined}
 ).
 
--define(OPENING_REPLY4,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:client"},
-	{xmlattr, undefined, undefined, 'id', "foobar"},
-	{xmlattr, ?NS_XML, undefined, 'lang', "fr"}
-], undefined}
+-define(OPENING_REPLY1a,
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'from', "dest"},
+      {xmlattr, undefined, undefined, 'id', "foobar"}
+    ], undefined}
 ).
 
--define(OPENING_REPLY5,
-{xmlnselement, ?NS_XMPP, "stream", 'stream', [
-	{xmlattr, undefined, undefined, 'xmlns', "jabber:client"},
-	{xmlattr, undefined, undefined, 'id', "foobar"},
-	{xmlattr, undefined, undefined, 'version', "1.0"}
-], undefined}
+-define(OPENING_REPLY2a,
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'version', "1.0"},
+      {xmlattr, undefined, undefined, 'from', "dest"},
+      {xmlattr, undefined, undefined, 'id', "foobar"}
+    ], undefined}
+).
+
+-define(OPENING_REPLY3a,
+  {xmlnselement, ?NS_XMPP, "stream", ?NS_JABBER_CLIENT, 'stream', [
+      {xmlattr, undefined, undefined, 'version', "1.0"},
+      {xmlattr, ?NS_XML, undefined, 'lang', "fr"},
+      {xmlattr, undefined, undefined, 'from', "dest"},
+      {xmlattr, undefined, undefined, 'id', "foobar"}
+    ], undefined}
 ).
 
 -define(CLOSING1,
-{xmlnsendelement, ?NS_XMPP, "stream", 'stream'}
+  {xmlnsendelement, ?NS_XMPP, "stream", 'stream'}
 ).
 
 test_stream_opening() ->
-	testsuite:is(exmpp_stream:opening([]),
-	    {error, unspecified_context}),
-	testsuite:is(exmpp_stream:opening([bad_arg]),
-	    {error, {unknown_argument, bad_arg}}),
-	testsuite:is(exmpp_stream:opening([{context, bad}]),
-	    {error, {unknown_context, bad}}),
-	testsuite:is(exmpp_stream:opening([
-	    {context, client}
-	    ]), ?OPENING1),
-	testsuite:is(exmpp_stream:opening([
-	    {context, server}
-	    ]), ?OPENING2),
-	testsuite:is(exmpp_stream:opening([
-	    {context, client},
-	    {to, "dest"}
-	    ]), ?OPENING3),
-	testsuite:is(exmpp_stream:opening([
-	    {context, client},
-	    {lang, "fr"}
-	    ]), ?OPENING4),
-	testsuite:is(exmpp_stream:opening([
-	    {context, client},
-	    {version, "1.0"}
-	    ]), ?OPENING5),
-	ok.
+    testsuite:is(exmpp_stream:opening("dest", ?NS_JABBER_CLIENT, undefined),
+      ?OPENING1),
+    testsuite:is(exmpp_stream:opening("dest", ?NS_JABBER_CLIENT, ""),
+      ?OPENING1),
+    testsuite:is(exmpp_stream:opening("dest", ?NS_JABBER_CLIENT, {0, 0}),
+      ?OPENING1),
+    testsuite:is(exmpp_stream:opening("dest", ?NS_JABBER_CLIENT, "1.0"),
+      ?OPENING2),
+    testsuite:is(exmpp_stream:opening("dest", ?NS_JABBER_CLIENT, {1, 0}),
+      ?OPENING2),
+    testsuite:is(exmpp_stream:opening("dest", ?NS_JABBER_CLIENT, "1.0", "fr"),
+      ?OPENING3),
+    ok.
 
 test_stream_opening_reply() ->
-	testsuite:is(exmpp_stream:opening_reply([]),
-	    {error, unspecified_context}),
-	testsuite:is(exmpp_stream:opening_reply([bad_arg]),
-	    {error, {unknown_argument, bad_arg}}),
-	testsuite:is(exmpp_stream:opening_reply([{context, bad}]),
-	    {error, {unknown_context, bad}}),
-	testsuite:is(exmpp_stream:opening_reply([
-	    {context, client},
-	    {id, "foobar"}
-	    ]), ?OPENING_REPLY1),
-	testsuite:is(exmpp_stream:opening_reply([
-	    {context, server},
-	    {id, "foobar"}
-	    ]), ?OPENING_REPLY2),
-	testsuite:is(exmpp_stream:opening_reply([
-	    {context, client},
-	    {id, "foobar"},
-	    {from, "orig"}
-	    ]), ?OPENING_REPLY3),
-	testsuite:is(exmpp_stream:opening_reply([
-	    {context, client},
-	    {id, "foobar"},
-	    {lang, "fr"}
-	    ]), ?OPENING_REPLY4),
-	testsuite:is(exmpp_stream:opening_reply([
-	    {context, client},
-	    {id, "foobar"},
-	    {version, "1.0"}
-	    ]), ?OPENING_REPLY5),
-	ok.
+    testsuite:is(exmpp_stream:opening_reply(
+        "dest", ?NS_JABBER_CLIENT, undefined, "foobar"),
+      ?OPENING_REPLY1),
+    testsuite:is(exmpp_stream:opening_reply(
+        "dest", ?NS_JABBER_CLIENT, "", "foobar"),
+      ?OPENING_REPLY1),
+    testsuite:is(exmpp_stream:opening_reply(
+        "dest", ?NS_JABBER_CLIENT, {0, 0}, "foobar"),
+      ?OPENING_REPLY1),
+    testsuite:is(exmpp_stream:opening_reply(
+        "dest", ?NS_JABBER_CLIENT, "1.0", "foobar"),
+      ?OPENING_REPLY2),
+    testsuite:is(exmpp_stream:opening_reply(
+        "dest", ?NS_JABBER_CLIENT, {1, 0}, "foobar"),
+      ?OPENING_REPLY2),
+    testsuite:is(exmpp_stream:opening_reply(
+        "dest", ?NS_JABBER_CLIENT, "1.0", "foobar", "fr"),
+      ?OPENING_REPLY3),
+    testsuite:is(exmpp_stream:opening_reply(?OPENING1, "foobar"),
+      ?OPENING_REPLY1a),
+    testsuite:is(exmpp_stream:opening_reply(?OPENING2, "foobar"),
+      ?OPENING_REPLY2a),
+    testsuite:is(exmpp_stream:opening_reply(?OPENING3, "foobar"),
+      ?OPENING_REPLY3a),
+    ok.
 
 test_stream_closing() ->
-	testsuite:is(exmpp_stream:closing(),
-	    ?CLOSING1),
-	testsuite:is(exmpp_stream:closing(?OPENING1),
-	    ?CLOSING1),
-	ok.
+    testsuite:is(exmpp_stream:closing(),
+      ?CLOSING1),
+    testsuite:is(exmpp_stream:closing(?OPENING1),
+      ?CLOSING1),
+    ok.
