596319c54d398c82f30bd96ed1d8b01eed980884
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-05-07 12:02:24 +0000

    o  Use 'receiving entity' instead of 'server'. o  Use 'initiating entity' instead of 'client'. o  Replace references to exmpp_server_stream with exmpp_stream. o  Avoid list concatenation when it's possible. o  Use exceptions for error handling. o  Rewrite error creation. o  In next_step/1, change {auth, Mech} by {auth, Mech, none} so that the 'auth' tuple has always the same size. o  Add basic documentation.
    SVN Revision: 358

--- exmpp/src/server/exmpp_server_sasl.erl
+++ exmpp/src/server/exmpp_server_sasl.erl
@@ -2,13 +2,27 @@
 
 %% @author Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
 
+%% @doc
+%% The module <strong>{@module}</strong> implements the receiving entity
+%% side of SASL authentication.
+%%
+%% <p>
+%% Note that it doesn't implement SASL, only feature negotiation at the
+%% XMPP level.
+%% </p>
+
 -module(exmpp_server_sasl).
 -vsn('$Revision$').
 
 -include("exmpp.hrl").
 
+% Feature announcement.
 -export([
-  feature/1,
+  feature/1
+]).
+
+% SASL exchange.
+-export([
   challenge/1,
   failure/0,
   failure/1,
@@ -19,18 +33,25 @@
 % Feature announcement.
 % --------------------------------------------------------------------
 
-feature(Mechanisms) ->
-    case mechanisms_list(Mechanisms) of
-        {error, Reason} ->
-            {error, Reason};
-        Children ->
-            #xmlnselement{
-              ns = ?NS_SASL,
-              name = 'mechanisms',
-              children = Children
-            }
-    end.
+%% @spec (Mechanisms) -> Feature
+%%     Mechanisms = [string()]
+%%     Feature = exmpp_xml:xmlnselement()
+%% @throws {sasl, feature_announcement, invalid_mechanisms_list, []} |
+%%         {sasl, feature_announcement, invalid_mechanism, Mechanism}
+%% @doc Make a feature announcement child.
+%%
+%% The result should then be passed to {@link exmpp_stream:features/1}.
 
+feature(Mechanisms) ->
+    Children = mechanisms_list(Mechanisms),
+    #xmlnselement{
+      ns = ?NS_SASL,
+      name = 'mechanisms',
+      children = Children
+    }.
+
+mechanisms_list([]) ->
+    throw({sasl, feature_announcement, invalid_mechanisms_list, []});
 mechanisms_list(Mechanisms) ->
     mechanisms_list2(Mechanisms, []).
 
@@ -43,16 +64,34 @@ mechanisms_list2([Mechanism | Rest], Children) ->
               children = []
             },
             mechanisms_list2(Rest,
-              Children ++ [exmpp_xml:set_cdata(Child, Mechanism)]);
+              [exmpp_xml:set_cdata(Child, Mechanism) | Children]);
         false ->
-            {error, bad_mechanisms_list}
+            throw({sasl, feature_announcement, invalid_mechanism, Mechanism})
     end;
 mechanisms_list2([], Children) ->
-    Children.
+    lists:reverse(Children).
 
 % --------------------------------------------------------------------
 % SASL exchange.
 % --------------------------------------------------------------------
+
+standard_conditions() ->
+    [
+      {'aborted'},
+      {'incorrect-encoding'},
+      {'invalid-authzid'},
+      {'invalid-mechanism'},
+      {'mechanism-too-weak'},
+      {'not-authorized'},
+      {'temporary-auth-failure'}
+    ].
+
+%% @spec (Challenge) -> Challenge_El
+%%     Challenge = string()
+%%     Challenge_El = exmpp_xml:xmlnselement()
+%% @doc Prepare a `<challenge/>' element with the given challenge.
+%%
+%% `Challenge' will be Base64-encoded by this function.
 
 challenge(Challenge) ->
     El = #xmlnselement{
@@ -62,6 +101,10 @@ challenge(Challenge) ->
     },
     exmpp_xml:set_cdata(El, exmpp_internals:encode_base64(Challenge)).
 
+%% @spec () -> Failure
+%%     Failure = exmpp_xml:xmlnselement()
+%% @doc Prepare a `<failure/>' element.
+
 failure() ->
     #xmlnselement{
       ns = ?NS_SASL,
@@ -69,18 +112,38 @@ failure() ->
       children = []
     }.
 
-failure(Reason) ->
-    El = #xmlnselement{
+%% @spec (Condition) -> Failure
+%%     Condition = atom()
+%%     Failure = exmpp_xml:xmlnselement()
+%% @doc Prepare a `<failure/>' element.
+
+failure(Condition) ->
+    case lists:keymember(Condition, 1, standard_conditions()) of
+        true  -> ok;
+        false -> throw({sasl, failure, invalid_condition, Condition})
+    end,
+    Condition_El = #xmlnselement{
       ns = ?NS_SASL,
-      name = list_to_atom(Reason),
+      name = Condition,
       children = []
     },
-    exmpp_xml:append_child(failure(), El).
+    exmpp_xml:append_child(failure(), Condition_El).
+
+%% @spec (El) -> Type
+%%     El = exmpp_xml:xmlnselement()
+%%     Type = Auth | Response | Abort
+%%     Auth = {auth, Mechanism, none | string()}
+%%     Response = {response, string()}
+%%     Abort = abort
+%% @throws {sasl, next_step, unexpected_element, El}
+%% @doc Extract the response that the initiating entity sent.
+%%
+%% Any response data is Base64-decoded.
 
 next_step(#xmlnselement{ns = ?NS_SASL, name = 'auth'} = El) ->
     Mechanism = exmpp_xml:get_attribute(El, 'mechanism'),
     case string:strip(exmpp_xml:get_cdata(El)) of
-        ""      -> {auth, Mechanism};
+        ""      -> {auth, Mechanism, none};
         "="     -> {auth, Mechanism, ""};
         Encoded -> {auth, Mechanism, exmpp_internals:decode_base64(Encoded)}
     end;
@@ -89,5 +152,5 @@ next_step(#xmlnselement{ns = ?NS_SASL, name = 'response'} = El) ->
     {response, exmpp_internals:decode_base64(Encoded)};
 next_step(#xmlnselement{ns = ?NS_SASL, name = 'abort'}) ->
     abort;
-next_step(#xmlnselement{}) ->
-    {error, unexpected_stanza}.
+next_step(El) ->
+    throw({sasl, next_step, unexpected_element, El}).
