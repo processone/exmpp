2a1494b4e2d6d48ba0b33bf076ca79dbd4193ba2
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-05-07 11:56:05 +0000

    o  Use 'receiving entity' instead of 'server'. o  Use 'initiating entity' instead of 'client'. o  Replace references to exmpp_server_stream with exmpp_stream. o  Use exceptions for error handling. o  Add basic documentation.
    SVN Revision: 357

--- src/client/exmpp_client_tls.erl
+++ src/client/exmpp_client_tls.erl
@@ -2,13 +2,27 @@
 
 %% @author Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
 
+%% @doc
+%% The module <strong>{@module}</strong> implements the initiating
+%% entity side of the TLS feature.
+%%
+%% <p>
+%% Note that it doesn't implement encryption, only feature negotiation
+%% at the XMPP level.
+%% </p>
+
 -module(exmpp_client_tls).
 -vsn('$Revision$').
 
 -include("exmpp.hrl").
 
+% Feature announcement.
 -export([
-  announced_support/1,
+  announced_support/1
+]).
+
+% TLS negotiation.
+-export([
   starttls/0
 ]).
 
@@ -16,23 +30,35 @@
 % Feature announcement.
 % --------------------------------------------------------------------
 
+%% @spec (Features_Announcement) -> Support
+%%     Features_Announcement = exmpp_xml:xmlnselement()
+%%     Support = none | optional | required
+%% @throws {tls, announced_support, invalid_announcement, El}
+%% @doc Return the kind of TLS negotiation the receiving entity asks for.
+
 announced_support(#xmlnselement{ns = ?NS_XMPP, name = 'features'} = El) ->
     case exmpp_xml:get_element_by_name(El, ?NS_TLS, 'starttls') of
-        false -> none;
-        Child -> announced_support(Child)
-    end;
-announced_support(#xmlnselement{ns = ?NS_TLS, name = 'starttls',
+        undefined -> none;
+        Child     -> announced_support2(Child)
+    end.
+
+announced_support2(#xmlnselement{ns = ?NS_TLS, name = 'starttls',
   children = []}) ->
     optional;
-announced_support(#xmlnselement{ns = ?NS_TLS, name = 'starttls',
+announced_support2(#xmlnselement{ns = ?NS_TLS, name = 'starttls',
   children = [#xmlnselement{ns = ?NS_TLS, name = 'required'}]}) ->
     required;
-announced_support(#xmlnselement{ns = ?NS_TLS, name = 'starttls'}) ->
-    invalid.
+announced_support2(#xmlnselement{ns = ?NS_TLS, name = 'starttls'} = El) ->
+    throw({tls, announced_support, invalid_announcement, El}).
 
 % --------------------------------------------------------------------
 % TLS negotiation.
 % --------------------------------------------------------------------
+
+%% @spec () -> STARTTLS
+%%     STARTTLS = exmpp_xml:xmlnselement()
+%% @doc Make an XML element to tell the receiving entity that we want to
+%% use TLS.
 
 starttls() ->
     #xmlnselement{
--- src/server/exmpp_server_tls.erl
+++ src/server/exmpp_server_tls.erl
@@ -2,14 +2,28 @@
 
 %% @author Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
 
+%% @doc
+%% The module <strong>{@module}</strong> implements the receiving entity
+%% side of the TLS feature.
+%%
+%% <p>
+%% Note that it doesn't implement encryption, only feature negotiation
+%% at the XMPP level.
+%% </p>
+
 -module(exmpp_server_tls).
 -vsn('$Revision$').
 
 -include("exmpp.hrl").
 
+% Feature announcement.
 -export([
   feature/0,
-  feature/1,
+  feature/1
+]).
+
+% TLS negotiation.
+-export([
   proceed/0,
   failure/0
 ]).
@@ -18,8 +32,25 @@
 % Feature announcement.
 % --------------------------------------------------------------------
 
+%% @spec () -> Feature
+%%     Feature = exmpp_xml:xmlnselement()
+%% @doc Make a feature announcement child.
+%%
+%% TLS is announced as not required.
+%%
+%% The result should then be passed to {@link exmpp_stream:features/1}.
+%%
+%% @see feature/1.
+
 feature() ->
     feature(false).
+
+%% @spec (Is_Required) -> Feature
+%%     Is_Required = bool()
+%%     Feature = exmpp_xml:xmlnselement()
+%% @doc Make a feature announcement child.
+%%
+%% The result should then be passed to {@link exmpp_stream:features/1}.
 
 feature(Is_Required) ->
     Feature = #xmlnselement{
@@ -43,12 +74,22 @@ feature(Is_Required) ->
 % TLS negotiation.
 % --------------------------------------------------------------------
 
+%% @spec () -> Proceed
+%%     Proceed = exmpp_xml:xmlnselement()
+%% @doc Make an XML element to tell the initiating entity it can proceed
+%% with the TLS handshake.
+
 proceed() ->
     #xmlnselement{
       ns = ?NS_TLS,
       name = 'proceed',
       children = []
     }.
+
+%% @spec () -> Failure
+%%     Failure = exmpp_xml:xmlnselement()
+%% @doc Make an XML element to tell the initiating entity that the TLS
+%% handshake failed.
 
 failure() ->
     #xmlnselement{
