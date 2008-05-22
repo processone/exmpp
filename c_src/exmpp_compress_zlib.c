c3a0ae3ede520d67dee48430b7417d0c83b6cce7
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-05-22 15:13:40 +0000

    Add exmpp_compress and its associated port driver.
    SVN Revision: 386

---
+++ c_src/exmpp_compress_zlib.c
@@ -0,0 +1,331 @@
+/* $Id$ */
+
+#include <string.h>
+#include <erl_driver.h>
+#include <ei.h>
+#include <zlib.h>
+
+#define	DRIVER_NAME	exmpp_compress_zlib
+#define	_S(s)		#s
+#define	S(s)		_S(s)
+
+#define	BUF_SIZE	1024
+
+/* Control operations. */
+enum {
+	COMMAND_SET_COMPRESS_METHOD = 1,
+	COMMAND_SET_COMPRESS_LEVEL,
+	COMMAND_PREPARE_COMPRESS,
+	COMMAND_PREPARE_UNCOMPRESS,
+	COMMAND_COMPRESS,
+	COMMAND_UNCOMPRESS,
+	COMMAND_SVN_REVISION
+};
+
+/* Driver data. */
+struct exmpp_compress_zlib_data {
+	/* Options. */
+	int		compress_level;
+	int		use_gzip;
+
+	z_stream	inf_z;
+	z_stream	def_z;
+};
+
+#define	SKIP_VERSION(buf, index, version)	do {			\
+	index = 0;							\
+	ei_decode_version(buf, &index, &version);			\
+} while (0)
+
+#define	NEW_SEND_BUF(to_send)						\
+	(to_send) = driver_alloc(sizeof(ei_x_buff));			\
+	if ((to_send) == NULL)						\
+		return (-1);						\
+	ei_x_new_with_version((to_send));
+
+#define	COPY_AND_FREE_BUF(to_send, size, b)				\
+	(size) = (to_send)->index + 1;					\
+	(b) = driver_alloc_binary((size));				\
+	(b)->orig_bytes[0] = 1;						\
+	memcpy((b)->orig_bytes + 1, (to_send)->buff,			\
+	    (to_send)->index);						\
+	ei_x_free((to_send));						\
+	driver_free((to_send));
+
+/* -------------------------------------------------------------------
+ * Erlang port driver callbacks.
+ * ------------------------------------------------------------------- */
+
+static ErlDrvData
+exmpp_compress_zlib_start(ErlDrvPort port, char *command)
+{
+	struct exmpp_compress_zlib_data *edd;
+
+	/* Set binary mode. */
+	set_port_control_flags(port, PORT_CONTROL_FLAG_BINARY);
+
+	/* Allocate driver data structure. */
+	edd = driver_alloc(sizeof(*edd));
+	if (edd == NULL)
+		return (NULL);
+
+	edd->use_gzip = 0;
+	edd->compress_level = Z_DEFAULT_COMPRESSION;
+
+	edd->inf_z.next_in  = edd->def_z.next_in  = 0;
+	edd->inf_z.avail_in = edd->def_z.avail_in = 0;
+	edd->inf_z.zalloc   = edd->def_z.zalloc   = NULL;
+	edd->inf_z.zfree    = edd->def_z.zfree    = NULL;
+	edd->inf_z.opaque   = edd->def_z.opaque   = NULL;
+
+	return (ErlDrvData)edd;
+}
+
+static void
+exmpp_compress_zlib_stop(ErlDrvData drv_data)
+{
+	struct exmpp_compress_zlib_data *edd;
+
+	edd = (struct exmpp_compress_zlib_data *)drv_data;
+
+	driver_free(edd);
+}
+
+static int
+exmpp_compress_zlib_control(ErlDrvData drv_data, unsigned int command,
+    char *buf, int len, char **rbuf, int rlen)
+{
+	struct exmpp_compress_zlib_data *edd;
+	int ret, index, version, type, type_size;
+	char atom[MAXATOMLEN];
+	size_t size;
+	long compress_level;
+	ErlDrvBinary *b;
+	ei_x_buff *to_send;
+	z_stream *z;
+
+	edd = (struct exmpp_compress_zlib_data *)drv_data;
+
+	size = 0;
+	b = NULL;
+
+	switch (command) {
+	case COMMAND_SET_COMPRESS_METHOD:
+		SKIP_VERSION(buf, index, version);
+
+		/* Get compression method. */
+		ei_decode_atom(buf, &index, atom);
+
+		if (strcmp(atom, "zlib") == 0) {
+			edd->use_gzip = 0;
+		} else if (strcmp(atom, "gzip") == 0) {
+			edd->use_gzip = 1;
+		} else {
+			/* Only zlib is supported by this port driver. */
+			NEW_SEND_BUF(to_send);
+			ei_x_encode_tuple_header(to_send, 2);
+			ei_x_encode_atom(to_send,
+			    "unsupported_compress_method");
+			ei_x_encode_string(to_send, atom);
+
+			COPY_AND_FREE_BUF(to_send, size, b);
+
+			break;
+		}
+
+		break;
+	case COMMAND_SET_COMPRESS_LEVEL:
+		SKIP_VERSION(buf, index, version);
+
+		/* Get the compression level. */
+		ei_get_type(buf, &index, &type, &type_size);
+		switch (type) {
+		case ERL_INTEGER_EXT:
+			ei_decode_long(buf, &index, &compress_level);
+
+			if (compress_level < Z_NO_COMPRESSION ||
+			    compress_level > Z_BEST_COMPRESSION) {
+				/* Valid levels are 0..9. */
+				NEW_SEND_BUF(to_send);
+				ei_x_encode_tuple_header(to_send, 2);
+				ei_x_encode_atom(to_send,
+				    "invalid_compress_level");
+				ei_x_encode_long(to_send, compress_level);
+
+				COPY_AND_FREE_BUF(to_send, size, b);
+
+				goto out;
+			}
+
+			edd->compress_level = compress_level;
+
+			break;
+		case ERL_ATOM_EXT:
+			ei_decode_atom(buf, &index, atom);
+
+			if (strcmp(atom, "default") != 0) {
+				/* Valid levels are 0..9. */
+				NEW_SEND_BUF(to_send);
+				ei_x_encode_tuple_header(to_send, 2);
+				ei_x_encode_atom(to_send,
+				    "invalid_compress_level");
+				ei_x_encode_atom(to_send, atom);
+
+				COPY_AND_FREE_BUF(to_send, size, b);
+
+				goto out;
+			}
+
+			break;
+		}
+
+		break;
+	case COMMAND_PREPARE_COMPRESS:
+	case COMMAND_PREPARE_UNCOMPRESS:
+		if (command == COMMAND_PREPARE_COMPRESS) {
+			if (edd->use_gzip)
+				ret = deflateInit2(&edd->def_z,
+				    edd->compress_level,
+				    Z_DEFLATED, 15 + 16, 8,
+				    Z_DEFAULT_STRATEGY);
+			else
+				ret = deflateInit(&edd->def_z,
+				    edd->compress_level);
+		} else {
+			if (edd->use_gzip)
+				ret = inflateInit2(&edd->inf_z,
+				    15 + 16);
+			else
+				ret = inflateInit(&edd->inf_z);
+		}
+
+		if (ret != Z_OK) {
+			NEW_SEND_BUF(to_send);
+			if (ret == Z_MEM_ERROR)
+				ei_x_encode_atom(to_send, "no_memory");
+			else if (ret == Z_VERSION_ERROR)
+				ei_x_encode_atom(to_send,
+				    "incompatible_zlib_version");
+			else
+				ei_x_encode_atom(to_send,
+				    "zlib_internal_error");
+
+			COPY_AND_FREE_BUF(to_send, size, b);
+
+			break;
+		}
+
+		break;
+	case COMMAND_COMPRESS:
+	case COMMAND_UNCOMPRESS:
+		size = BUF_SIZE + 1;
+		rlen = 1;
+		b = driver_alloc_binary(size);
+		b->orig_bytes[0] = 0;
+
+		z = NULL;
+		if (command == COMMAND_COMPRESS)
+			z = &edd->def_z;
+		else
+			z = &edd->inf_z;
+
+		z->next_in = (unsigned char *)buf;
+		z->avail_in = len;
+		z->avail_out = 0;
+		ret = Z_OK;
+
+		while (ret == Z_OK && z->avail_out == 0) {
+			z->next_out = (unsigned char *)b->orig_bytes + rlen;
+			z->avail_out = BUF_SIZE;
+
+			if (command == COMMAND_COMPRESS) {
+				ret = deflate(z, Z_SYNC_FLUSH);
+				if (ret != Z_OK && ret != Z_STREAM_END) {
+					driver_free_binary(b);
+
+					NEW_SEND_BUF(to_send);
+					ei_x_encode_atom(to_send,
+					    "deflate_error");
+
+					COPY_AND_FREE_BUF(to_send, size, b);
+
+					goto out;
+				}
+			} else {
+				ret = inflate(z, Z_SYNC_FLUSH);
+				if (ret != Z_OK && ret != Z_STREAM_END) {
+					driver_free_binary(b);
+
+					NEW_SEND_BUF(to_send);
+					ei_x_encode_atom(to_send,
+					    "inflate_error");
+
+					COPY_AND_FREE_BUF(to_send, size, b);
+
+					goto out;
+				}
+			}
+
+			rlen += (BUF_SIZE - z->avail_out);
+			size += (BUF_SIZE - z->avail_out);
+			b = driver_realloc_binary(b, size);
+		}
+
+		size = rlen;
+		b = driver_realloc_binary(b, size);
+
+		break;
+	case COMMAND_SVN_REVISION:
+		/* Store the revision in the buffer. */
+		NEW_SEND_BUF(to_send);
+		ei_x_encode_string(to_send, "$Revision$");
+
+		COPY_AND_FREE_BUF(to_send, size, b);
+
+		break;
+	default:
+		/* Commad not recognized. */
+		NEW_SEND_BUF(to_send);
+		ei_x_encode_tuple_header(to_send, 2);
+		ei_x_encode_atom(to_send, "unknown_command");
+		ei_x_encode_ulong(to_send, command);
+
+		COPY_AND_FREE_BUF(to_send, size, b);
+	}
+
+out:
+	if (b == NULL) {
+		size = 1;
+		b = driver_alloc_binary(size);
+		b->orig_bytes[0] = 0;
+	}
+
+	*rbuf = (char *)b;
+
+	return (size);
+}
+
+/* -------------------------------------------------------------------
+ * Driver declaration.
+ * ------------------------------------------------------------------- */
+
+static ErlDrvEntry compress_zlib_driver_entry = {
+	NULL,				/* init */
+	exmpp_compress_zlib_start,	/* start */
+	exmpp_compress_zlib_stop,	/* stop */
+	NULL,				/* output */
+	NULL,				/* ready_input */
+	NULL,				/* ready_output */
+	S(DRIVER_NAME),			/* driver name */
+	NULL,				/* finish */
+	NULL,				/* handle */
+	exmpp_compress_zlib_control,	/* control */
+	NULL,				/* timeout */
+	NULL				/* outputv */
+};
+
+DRIVER_INIT(DRIVER_NAME)
+{
+
+	return &compress_zlib_driver_entry;
+}
---
+++ src/core/exmpp_compress.erl
@@ -0,0 +1,577 @@
+% $Id$
+
+%% @author Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
+
+%% @doc
+%% The module <strong>{@module}</strong> provides functions to handle
+%% stream compression.
+
+-module(exmpp_compress).
+-vsn('$Revision$').
+
+-behaviour(gen_server).
+
+% Initialization.
+-export([
+  start/0,
+  start_link/0
+]).
+
+% Registry handling.
+-export([
+  register_engine/3,
+  register_engine/4,
+  get_compress_methods/0,
+  get_engine_names/0,
+  get_engine_names/1,
+  get_prefered_engine_name/1,
+  get_engine_driver/1
+]).
+
+% Compression activation.
+-export([
+  compress/2
+]).
+
+% Common socket API.
+-export([
+  send/2,
+  recv/2,
+  recv/3,
+  controlling_process/2,
+  close/1,
+  port_revision/1
+]).
+
+% Communication with the underlying module/socket.
+-export([
+  underlying_send/2,
+  underlying_recv/2
+]).
+
+% gen_server(3erl) callbacks.
+-export([
+  init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3
+]).
+
+-record(state, {
+  engines,
+  by_compress_method
+}).
+
+-record(compress_engine, {
+  name,
+  driver_path,
+  driver,
+  compress_methods = []
+}).
+
+-record(compress_socket, {
+  socket,
+  packet_mode = binary,
+  port
+}).
+
+-define(SERVER, ?MODULE).
+-define(DEFAULT_ENGINE, zlib).
+
+-define(COMMAND_SET_COMPRESS_METHOD, 1).
+-define(COMMAND_SET_COMPRESS_LEVEL,  2).
+-define(COMMAND_PREPARE_COMPRESS,    3).
+-define(COMMAND_PREPARE_UNCOMPRESS,  4).
+-define(COMMAND_COMPRESS,            5).
+-define(COMMAND_UNCOMPRESS,          6).
+-define(COMMAND_SVN_REVISION,        7).
+
+% --------------------------------------------------------------------
+% Initialization.
+% --------------------------------------------------------------------
+
+%% @hidden
+
+start() ->
+    gen_server:start({local, ?SERVER}, ?MODULE, [], []),
+    register_builtin_engines().
+
+%% @hidden
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []),
+    register_builtin_engines().
+
+-ifdef(HAVE_ZLIB).
+-define(REGISTER_ZLIB,
+  register_engine(zlib, exmpp_compress_zlib, [{zlib, 10}, {gzip, 10}])).
+-else.
+-define(REGISTER_ZLIB, ok).
+-endif.
+
+register_builtin_engines() ->
+    ?REGISTER_ZLIB,
+    ok.
+
+% --------------------------------------------------------------------
+% Registry handling.
+% --------------------------------------------------------------------
+
+%% @spec (Name, Driver, Compress_Methods) -> ok
+%%     Name = atom()
+%%     Driver = atom()
+%%     Compress_Mehods = [{atom(), Priority}]
+%%     Priority = integer()
+%% @doc Add a new compression engine.
+
+register_engine(Name, Driver, Compress_Methods) ->
+    register_engine(Name, undefined, Driver, Compress_Methods).
+
+%% @spec (Name, Driver_Path, Driver, Compress_Methods) -> ok
+%%     Name = atom()
+%%     Driver_Path = string()
+%%     Driver = atom()
+%%     Compress_Mehods = [{atom(), Priority}]
+%%     Priority = integer()
+%% @doc Add a new compression engine.
+
+register_engine(Name, Driver_Path, Driver, Compress_Methods)
+  when is_atom(Name), is_list(Compress_Methods), length(Compress_Methods) > 0 ->
+    Engine = #compress_engine{
+      name = Name,
+      driver_path = Driver_Path,
+      driver = Driver,
+      compress_methods = Compress_Methods
+    },
+    gen_server:cast(?SERVER, {register_engine, Engine}).
+
+%% @spec () -> [Compress_Method]
+%%     Compress_Method = atom()
+%% @doc Return the list of supported compress methods.
+
+get_compress_methods() ->
+    gen_server:call(?SERVER, get_compress_methods).
+
+%% @spec () -> [Engine_Name]
+%%     Engine_Name = atom()
+%% @doc Return the list of compression engines.
+
+get_engine_names() ->
+    gen_server:call(?SERVER, get_engine_names).
+
+%% @spec (Compress_Method) -> [Engine_Name]
+%%     Compress_Method = atom()
+%%     Engine_Name = atom()
+%% @doc Return the list of compression engines which support the given compress method.
+%%
+%% The list is sorted from the most to the least prefered engine.
+
+get_engine_names(Compress_Method) ->
+    Engines = gen_server:call(?SERVER, {get_engines, Compress_Method}),
+    [E#compress_engine.name || E <- Engines].
+
+%% @spec (Compress_Method) -> [Engine_Name]
+%%     Compress_Method = atom()
+%%     Engine_Name = atom()
+%% @doc Return the name of the prefered compression engines which support the
+%% given compress method.
+
+get_prefered_engine_name(Compress_Method) ->
+    case get_prefered_engine(Compress_Method) of
+        undefined -> undefined;
+        Engine    -> Engine#compress_engine.name
+    end.
+
+get_prefered_engine(Compress_Method) ->
+    Engines = gen_server:call(?SERVER, {get_engines, Compress_Method}),
+    case Engines of
+        []           -> undefined;
+        [Engine | _] -> Engine
+    end.
+
+%% @spec (Engine_Name) -> Driver
+%%     Engine_Name = atom()
+%%     Driver = Driver_Name | {Driver_Path, Driver_Name}
+%%     Driver_Name = atom()
+%%     Driver_Path = string()
+%% @doc Return the port driver name associated to the given engine.
+
+get_engine_driver(Engine_Name) ->
+    case gen_server:call(?SERVER, {get_engine, Engine_Name}) of
+        undefined ->
+            undefined;
+        #compress_engine{driver_path = undefined, driver = Driver_Name} ->
+            Driver_Name;
+        #compress_engine{driver_path = Driver_Path, driver = Driver_Name} ->
+            {Driver_Path, Driver_Name}
+    end.
+
+% --------------------------------------------------------------------
+% Compression activation.
+% --------------------------------------------------------------------
+
+%% @spec (Socket_Desc, Options) -> Compress_Socket
+%%     Socket_Desc = {Mod, Socket}
+%%     Mod = atom()
+%%     Socket = term()
+%%     Options = [Option]
+%%     Option = {compress_method, Method} | {engine, Engine} | {mode, Mode} | {compress_level, Level}
+%%     Method = atom()
+%%     Engine = atom()
+%%     Mode = binary | list
+%%     Level = integer()
+%%     Compress_Socket = compress_socket()
+%% @doc Enable compression over the given socket.
+
+compress(Socket_Desc, Options) ->
+    % Start a port driver instance.
+    Driver_Name = get_engine_from_options(Options),
+    Port = try
+        exmpp_internals:open_port(Driver_Name)
+    catch
+        Exception1 ->
+            exmpp_internals:unload_driver(Driver_Name),
+            throw(Exception1)
+    end,
+
+    % Initialize the port and handshake.
+    try
+        % Set compression method.
+        case get_compress_method_from_options(Options) of
+            undefined -> ok;
+            CM        -> engine_set_compress_method(Port, CM)
+        end,
+
+        % Set compression level.
+        case get_compress_level_from_options(Options) of
+            undefined -> ok;
+            Level     -> engine_set_compress_level(Port, Level)
+        end,
+
+        % Packet mode.
+        Mode = get_packet_mode_from_options(Options),
+
+        % Enable compression.
+        engine_prepare_compress(Port),
+        engine_prepare_uncompress(Port),
+        #compress_socket{socket = Socket_Desc, packet_mode = Mode, port = Port}
+    catch
+        Exception2 ->
+            exmpp_internals:close_port(Port),
+            exmpp_internals:unload_driver(Driver_Name),
+            throw(Exception2)
+    end.
+
+% --------------------------------------------------------------------
+% Activation helpers.
+% --------------------------------------------------------------------
+
+get_engine_from_options(Options) ->
+    Engine_Name = case lists:keysearch(engine, 1, Options) of
+        {value, {_, Engine}} ->
+            Engine;
+        _ ->
+            case lists:keysearch(compress_method, 1, Options) of
+                {value, {_, CM}} ->
+                    get_prefered_engine_name(CM);
+                _ ->
+                    ?DEFAULT_ENGINE
+            end
+    end,
+    case get_engine_driver(Engine_Name) of
+        {Driver_Path, Driver_Name} ->
+            exmpp_internals:load_driver(Driver_Name, [Driver_Path]),
+            Driver_Name;
+        Driver_Name ->
+            exmpp_internals:load_driver(Driver_Name),
+            Driver_Name
+    end.
+
+get_compress_method_from_options(Options) ->
+    case lists:keysearch(compress_method, 1, Options) of
+        {value, {_, CM}} -> CM;
+        _                -> undefined
+    end.
+
+get_compress_level_from_options(Options) ->
+    case lists:keysearch(compress_level, 1, Options) of
+        {value, {_, Level}} when is_integer(Level) -> Level;
+        _                                          -> default
+    end.
+
+get_packet_mode_from_options(Options) ->
+    case lists:keysearch(packet_mode, 1, Options) of
+        {value, {_, binary}} -> binary;
+        {value, {_, list}}   -> list;
+        _                    -> binary
+    end.
+
+% --------------------------------------------------------------------
+% Common socket API.
+% --------------------------------------------------------------------
+
+%% @spec (Compress_Socket, Packet) -> ok | {error, Reason}
+%%     Compress_Socket = compress_socket()
+%%     Packet = binary() | list()
+%%     Reason = term()
+%% @doc Send `Packet' over a compressed connection.
+
+send(#compress_socket{socket = Socket_Desc, port = Port}, Packet) ->
+    try
+        Compressed = engine_compress(Port, Packet),
+        underlying_send(Socket_Desc, Compressed)
+    catch
+        Exception ->
+            {error, Exception}
+    end.
+
+%% @spec (Compress_Socket, Length) -> {ok, Packet} | {error, Reason}
+%%     Compress_Socket = compress_socket()
+%%     Length = integer()
+%%     Packet = binary() | list()
+%%     Reason = term()
+%% @doc Receive data over a compressed connection.
+%%
+%% `Length' is currently ignored.
+
+recv(Socket_Data, Length) ->
+    recv(Socket_Data, Length, infinity).
+
+%% @spec (Compress_Socket, Length, Timeout) -> {ok, Packet} | {error, Reason}
+%%     Compress_Socket = compress_socket()
+%%     Length = integer()
+%%     Timeout = integer()
+%%     Packet = binary() | list()
+%%     Reason = term()
+%% @doc Receive data over a compressed connection.
+%%
+%% `Length' is currently ignored.
+
+recv(#compress_socket{socket = Socket_Desc, packet_mode = Mode, port = Port},
+  _Length, Timeout) ->
+    try
+        case underlying_recv(Socket_Desc, Timeout) of
+            {ok, Packet} ->
+                Uncompressed = engine_uncompress(Port, Packet),
+                case Mode of
+                    binary -> {ok, Uncompressed};
+                    list   -> {ok, binary_to_list(Uncompressed)}
+                end;
+            {error, Reason} ->
+                {error, Reason}
+        end
+    catch
+        Exception ->
+            {error, Exception}
+    end.
+
+%% @spec (Compress_Socket, Pid) -> ok | {error, Reason}
+%%     Compress_Socket = compress_socket()
+%%     Pid = pid()
+%%     Reason = term()
+%% @doc Change the controlling socket of the underlying socket.
+
+controlling_process(#compress_socket{socket = {Mod, Socket}}, Pid) ->
+    Mod:controlling_process(Socket, Pid).
+
+%% @spec (Compress_Socket) -> ok | {error, Reason}
+%%     Compress_Socket = compress_socket()
+%%     Reason = term()
+%% @doc Close the underlying socket.
+
+close(#compress_socket{socket = {Mod, Socket}}) ->
+    % Close the underlying socket.
+    Mod:close(Socket).
+
+%% @hidden
+
+port_revision(#compress_socket{port = Port}) ->
+    engine_svn_revision(Port).
+
+% --------------------------------------------------------------------
+% Engine function wrappers.
+% --------------------------------------------------------------------
+
+control(Port, Command, Data) ->
+    case port_control(Port, Command, Data) of
+        <<0, Result/binary>> -> Result;
+        <<1, Error/binary>>  -> {error, binary_to_term(Error)}
+    end.
+
+engine_set_compress_method(Port, Method) ->
+    case control(Port, ?COMMAND_SET_COMPRESS_METHOD,
+      term_to_binary(Method)) of
+        {error, Reason} ->
+            throw({compress, compress, set_compress_method, Reason});
+        _ ->
+            ok
+    end.
+
+engine_set_compress_level(Port, Level) ->
+    case control(Port, ?COMMAND_SET_COMPRESS_LEVEL,
+      term_to_binary(Level)) of
+        {error, Reason} ->
+            throw({compress, compress, set_compress_level, Reason});
+        _ ->
+            ok
+    end.
+
+engine_prepare_compress(Port) ->
+    case control(Port, ?COMMAND_PREPARE_COMPRESS, <<>>) of
+        {error, Reason} ->
+            throw({compress, compress, prepare_compress, Reason});
+        _ ->
+            ok
+    end.
+
+engine_prepare_uncompress(Port) ->
+    case control(Port, ?COMMAND_PREPARE_UNCOMPRESS, <<>>) of
+        {error, Reason} ->
+            throw({compress, compress, prepare_uncompress, Reason});
+        _ ->
+            ok
+    end.
+
+engine_compress(Port, Data) when is_list(Data) ->
+    engine_compress(Port, list_to_binary(Data));
+engine_compress(_Port, <<>>) ->
+    <<>>;
+engine_compress(Port, Data) ->
+    case control(Port, ?COMMAND_COMPRESS, Data) of
+        {error, Reason} ->
+            throw({compress, compress, do_compress, Reason});
+        Result ->
+            Result
+    end.
+
+engine_uncompress(Port, Data) when is_list(Data) ->
+    engine_uncompress(Port, list_to_binary(Data));
+engine_uncompress(_Port, <<>>) ->
+    <<>>;
+engine_uncompress(Port, Data) ->
+    case control(Port, ?COMMAND_UNCOMPRESS, Data) of
+        {error, Reason} ->
+            throw({compress, uncompress, do_uncompress, Reason});
+        Result ->
+            Result
+    end.
+
+engine_svn_revision(Port) ->
+    case control(Port, ?COMMAND_SVN_REVISION, <<>>) of
+        {error, Reason} ->
+            throw({compress, handshake, svn_revision, Reason});
+        Revision ->
+            binary_to_term(Revision)
+    end.
+
+% --------------------------------------------------------------------
+% Communication with the underlying module/socket.
+% --------------------------------------------------------------------
+
+%% @hidden
+
+underlying_recv({Mod, Socket}, Timeout) ->
+    Mod:recv(Socket, 0, Timeout).
+
+%% @hidden
+
+underlying_send({Mod, Socket}, Packet) ->
+    Mod:send(Socket, Packet).
+
+% --------------------------------------------------------------------
+% gen_server(3erl) callbacks.
+% --------------------------------------------------------------------
+
+%% @hidden
+
+init([]) ->
+    Engines = dict:new(),
+    By_CM = dict:new(),
+    {ok, #state{engines = Engines, by_compress_method = By_CM}}.
+
+%% @hidden
+
+handle_call(get_compress_methods, _From,
+  #state{by_compress_method = By_CM} = State) ->
+    {reply, dict:fetch_keys(By_CM), State};
+
+handle_call(get_engine_names, _From,
+  #state{engines = Engines} = State) ->
+    {reply, dict:fetch_keys(Engines), State};
+
+handle_call({get_engines, CM}, _From,
+  #state{by_compress_method = By_CM} = State) ->
+    case dict:is_key(CM, By_CM) of
+        true  -> {reply, [E || {E, _P} <- dict:fetch(CM, By_CM)], State};
+        false -> {reply, [], State}
+    end;
+
+handle_call({get_engine, Engine_Name}, _From,
+  #state{engines = Engines} = State) ->
+    case dict:is_key(Engine_Name, Engines) of
+        true  -> {reply, dict:fetch(Engine_Name, Engines), State};
+        false -> {reply, undefined, State}
+    end;
+
+handle_call(Request, From, State) ->
+    error_logger:info_msg("~p:handle_call/3:~n- Request: ~p~n- From: ~p~n"
+      "- State: ~p~n", [?MODULE, Request, From, State]),
+    {reply, ok, State}.
+
+%% @hidden
+
+handle_cast({register_engine,
+  #compress_engine{name = Name, compress_methods = Compress_Methods} = Engine},
+  #state{engines = Engines, by_compress_method = By_CM} = State) ->
+    % Add engine to the global list.
+    New_Engines = dict:store(Name, Engine, Engines),
+    % Index engine by its compress methods.
+    Fun = fun({CM, Prio}, {E, CM_Dict}) ->
+        New_CM_Dict = case dict:is_key(CM, CM_Dict) of
+            true ->
+                L = [{E, Prio} | dict:fetch(CM, CM_Dict)],
+                New_L = lists:keysort(2, L),
+                dict:store(CM, New_L, CM_Dict);
+            false ->
+                dict:store(CM, [{E, Prio}], CM_Dict)
+        end,
+        {E, New_CM_Dict}
+    end,
+    {_, New_By_CM} = lists:foldl(Fun, {Engine, By_CM}, Compress_Methods),
+    {noreply, State#state{
+      engines = New_Engines,
+      by_compress_method = New_By_CM
+    }};
+
+handle_cast(Request, State) ->
+    error_logger:info_msg("~p:handle_cast/2:~n- Request: ~p~n"
+      "- State: ~p~n", [?MODULE, Request, State]),
+    {noreply, State}.
+
+%% @hidden
+
+handle_info(Info, State) ->
+    error_logger:info_msg("~p:handle_info/2:~n- Info: ~p~n"
+      "- State: ~p~n", [?MODULE, Info, State]),
+    {noreply, State}.
+
+%% @hidden
+
+code_change(Old_Vsn, State, Extra) ->
+    error_logger:info_msg("~p:code_change/3:~n- Old_Vsn: ~p~n- Extra: ~p~n"
+      "- State: ~p~n", [?MODULE, Old_Vsn, Extra, State]),
+    {ok, State}.
+
+%% @hidden
+
+terminate(_Reason, _State) ->
+    ok.
+
+% --------------------------------------------------------------------
+% Documentation / type definitions.
+% --------------------------------------------------------------------
+
+%% @type compress_socket().
+%% Compression socket obtained with {@link compress/2}.
