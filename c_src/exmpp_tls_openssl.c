7002a7d2d1e3a8d1fcda3e2d1bd6acc1c62941e5
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-05-23 16:30:13 +0000

    Add preliminary support for certificate verification (WORK IN PROGRESS). For now, it understands the basic verification made by OpenSSL and check the expected_id against subjectAltName.dNSName (not exactly as specified in RFC 2818 though). It still lacks the check against the XMPP-specific subjectAltName extension and the commonName.
    Also included:
    o  Add support for options.
    o  Fix a bug with the COMMAND_SET_PEER_VERIF command where expected_id
    was always set to the empty string when the caller specified a boolean
    instead.
    
    SVN Revision: 394

--- c_src/exmpp_tls_openssl.c
+++ c_src/exmpp_tls_openssl.c
@@ -5,36 +5,15 @@
 #include <ei.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <openssl/x509v3.h>
+
+#include "exmpp_tls.h"
 
 #define	DRIVER_NAME	exmpp_tls_openssl
 #define	_S(s)		#s
 #define	S(s)		_S(s)
 
 #define	BUF_SIZE	1024
-
-/* Control operations. */
-enum {
-	COMMAND_SET_MODE = 1,
-	COMMAND_SET_IDENTITY,
-	COMMAND_SET_PEER_VERIF,
-	COMMAND_SET_TRUSTED_CERTS,
-	COMMAND_SET_OPTIONS,
-	COMMAND_PREPARE_HANDSHAKE,
-	COMMAND_HANDSHAKE,
-	COMMAND_SET_ENCRYPTED_INPUT,
-	COMMAND_GET_DECRYPTED_INPUT,
-	COMMAND_SET_DECRYPTED_OUTPUT,
-	COMMAND_GET_ENCRYPTED_OUTPUT,
-	COMMAND_SHUTDOWN,
-	COMMAND_SVN_REVISION
-};
-
-/* Mode. */
-enum {
-	TLS_MODE_UNKNOWN = 0,
-	TLS_MODE_SERVER,
-	TLS_MODE_CLIENT
-};
 
 /* Driver data. */
 struct exmpp_tls_openssl_data {
@@ -49,7 +28,12 @@ struct exmpp_tls_openssl_data {
 	char		*expected_id;
 
 	/* Options. */
+	char		*trusted_certs;
 	int		 peer_cert_required;
+	int		 accept_expired_cert;
+	int		 accept_revoked_cert;
+	int		 accept_non_trusted_cert;
+	int		 accept_corrupted_cert;
 
 	SSL_CTX		*ctx;
 	SSL		*ssl;
@@ -60,6 +44,8 @@ struct exmpp_tls_openssl_data {
 static int	init_library(struct exmpp_tls_openssl_data *edd,
 		    ei_x_buff **to_send, size_t *size, ErlDrvBinary **b);
 static int	verify_callback(int preverify_ok, X509_STORE_CTX *x509_ctx);
+
+static int	ssl_ex_index;
 
 #define	SKIP_VERSION(buf, index, version)	do {			\
 	index = 0;							\
@@ -102,7 +88,12 @@ exmpp_tls_openssl_start(ErlDrvPort port, char *command)
 	edd->certificate = edd->private_key = NULL;
 	edd->verify_peer = 0;
 	edd->expected_id = NULL;
+	edd->trusted_certs = NULL;
 	edd->peer_cert_required = 0;
+	edd->accept_expired_cert = 0;
+	edd->accept_revoked_cert = 0;
+	edd->accept_non_trusted_cert = 0;
+	edd->accept_corrupted_cert = 0;
 
 	edd->ctx = NULL;
 	edd->ssl = NULL;
@@ -132,8 +123,8 @@ exmpp_tls_openssl_control(ErlDrvData drv_data, unsigned int command,
     char *buf, int len, char **rbuf, int rlen)
 {
 	struct exmpp_tls_openssl_data *edd;
-	int ret, index, version, arity, type, type_size;
-	char auth_method[MAXATOMLEN];
+	int ret, index, version, arity, type, type_size, flag;
+	char atom[MAXATOMLEN];
 	size_t size;
 	long mode;
 	unsigned long data_len;
@@ -159,13 +150,13 @@ exmpp_tls_openssl_control(ErlDrvData drv_data, unsigned int command,
 
 		/* Get auth method. */
 		ei_decode_tuple_header(buf, &index, &arity);
-		ei_decode_atom(buf, &index, auth_method);
-		if (strcmp(auth_method, "x509") != 0) {
+		ei_decode_atom(buf, &index, atom);
+		if (strcmp(atom, "x509") != 0) {
 			/* Only X.509 is supported by this port driver. */
 			NEW_SEND_BUF(to_send);
 			ei_x_encode_tuple_header(to_send, 2);
 			ei_x_encode_atom(to_send, "unsupported_auth_method");
-			ei_x_encode_string(to_send, auth_method);
+			ei_x_encode_string(to_send, atom);
 
 			COPY_AND_FREE_BUF(to_send, size, b);
 
@@ -197,6 +188,7 @@ exmpp_tls_openssl_control(ErlDrvData drv_data, unsigned int command,
 		case ERL_ATOM_EXT:
 			/* The peer will be checked by OpenSSL. */
 			ei_decode_boolean(buf, &index, &(edd->verify_peer));
+			break;
 		case ERL_STRING_EXT:
 			/* The peer will be checked by OpenSSL, then
 			 * the certificate will be compared to the
@@ -206,6 +198,63 @@ exmpp_tls_openssl_control(ErlDrvData drv_data, unsigned int command,
 				return (-1);
 			ei_decode_string(buf, &index, edd->expected_id);
 			edd->verify_peer = 1;
+			break;
+		}
+
+		break;
+	case COMMAND_SET_TRUSTED_CERTS:
+		SKIP_VERSION(buf, index, version);
+
+		/* Get auth method. */
+		ei_decode_tuple_header(buf, &index, &arity);
+		ei_decode_atom(buf, &index, atom);
+		if (strcmp(atom, "x509") != 0) {
+			/* Only X.509 is supported by this port driver. */
+			NEW_SEND_BUF(to_send);
+			ei_x_encode_tuple_header(to_send, 2);
+			ei_x_encode_atom(to_send, "unsupported_auth_method");
+			ei_x_encode_string(to_send, atom);
+
+			COPY_AND_FREE_BUF(to_send, size, b);
+
+			break;
+		}
+
+		/* Get the filename for the trusted certificates. */
+		ei_get_type(buf, &index, &type, &type_size);
+		edd->trusted_certs = driver_alloc(type_size + 1);
+		if (edd->trusted_certs == NULL)
+			return (-1);
+		ei_decode_string(buf, &index, edd->trusted_certs);
+
+		break;
+	case COMMAND_SET_OPTIONS:
+		SKIP_VERSION(buf, index, version);
+
+		/* Get auth method. */
+		ei_decode_tuple_header(buf, &index, &arity);
+		ei_decode_atom(buf, &index, atom);
+		ei_decode_boolean(buf, &index, &flag);
+
+		if (strcmp(atom, "peer_cert_required") == 0)
+			edd->peer_cert_required = flag;
+		else if (strcmp(atom, "accept_expired_cert") == 0)
+			edd->accept_expired_cert = flag;
+		else if (strcmp(atom, "accept_non_trusted_cert") == 0)
+			edd->accept_non_trusted_cert = flag;
+		else if (strcmp(atom, "accept_revoked_cert") == 0)
+			edd->accept_revoked_cert = flag;
+		else if (strcmp(atom, "accept_corrupted_cert") == 0)
+			edd->accept_corrupted_cert = flag;
+		else {
+			NEW_SEND_BUF(to_send);
+			ei_x_encode_tuple_header(to_send, 2);
+			ei_x_encode_atom(to_send, "unsupported_option");
+			ei_x_encode_atom(to_send, atom);
+
+			COPY_AND_FREE_BUF(to_send, size, b);
+
+			break;
 		}
 
 		break;
@@ -444,6 +493,21 @@ init_library(struct exmpp_tls_openssl_data *edd,
 	    SSL_VERIFY_FAIL_IF_NO_PEER_CERT : 0;
 	SSL_CTX_set_verify(edd->ctx, verify, verify_callback);
 
+	/* Set trusted certificates. */
+	if (edd->trusted_certs != NULL) {
+		ret = SSL_CTX_load_verify_locations(edd->ctx,
+		    edd->trusted_certs, NULL);
+		if (ret != 1) {
+			NEW_SEND_BUF(*to_send);
+			ei_x_encode_atom(*to_send,
+			    "load_trusted_certs_failed");
+
+			COPY_AND_FREE_BUF(*to_send, *size, *b);
+
+			goto err;
+		}
+	}
+
 	/* Create an SSL connection handle. */
 	edd->ssl = SSL_new(edd->ctx);
 	if (edd->ssl == NULL) {
@@ -471,6 +535,9 @@ init_library(struct exmpp_tls_openssl_data *edd,
 		break;
 	}
 
+	/* Add our own data to SSL. This will be used by verify_callback. */
+	SSL_set_ex_data(edd->ssl, ssl_ex_index, edd);
+
 	return (0);
 
 err:
@@ -485,11 +552,150 @@ err:
 static int
 verify_callback(int preverify_ok, X509_STORE_CTX *x509_ctx)
 {
+	SSL *ssl;
+	struct exmpp_tls_openssl_data *edd;
+	int err, depth, gens_count, i;
+	X509 *cert;
+	STACK_OF(GENERAL_NAME) *gens;
+	const GENERAL_NAME *gen;
+	char *dnsname;
 
-	/* TODO: Implements a real verification. For now, any certificate
-	 * is accepted. */
+	/* Get the port driver's private data. We need it to access
+	 * verification options. */
+	ssl = X509_STORE_CTX_get_ex_data(x509_ctx,
+	    SSL_get_ex_data_X509_STORE_CTX_idx());
+	edd = SSL_get_ex_data(ssl, ssl_ex_index);
 
-	return (1);
+	if (!preverify_ok) {
+		/* The verification done by OpenSSL failed. We check if the
+		 * error is acceptable for the user. */
+
+		/* Get the verification error code. */
+		err = X509_STORE_CTX_get_error(x509_ctx);
+
+		/* Is this kind of error is accepted? */
+		switch (err) {
+		case X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT:
+		case X509_V_ERR_UNABLE_TO_GET_CRL:
+		case X509_V_ERR_CERT_SIGNATURE_FAILURE:
+		case X509_V_ERR_CRL_SIGNATURE_FAILURE:
+		case X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT:
+		case X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN:
+		case X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY:
+		case X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE:
+		case X509_V_ERR_CERT_CHAIN_TOO_LONG:
+		case X509_V_ERR_INVALID_CA:
+		case X509_V_ERR_PATH_LENGTH_EXCEEDED:
+		case X509_V_ERR_INVALID_PURPOSE:
+		case X509_V_ERR_CERT_UNTRUSTED:
+		case X509_V_ERR_CERT_REJECTED:
+		case X509_V_ERR_SUBJECT_ISSUER_MISMATCH:
+		case X509_V_ERR_AKID_SKID_MISMATCH:
+		case X509_V_ERR_AKID_ISSUER_SERIAL_MISMATCH:
+		case X509_V_ERR_KEYUSAGE_NO_CERTSIGN:
+			/* The peer certificate isn't trusted. */
+			if (!edd->accept_non_trusted_cert)
+				return (0);
+			break;
+		case X509_V_ERR_CERT_NOT_YET_VALID:
+		case X509_V_ERR_CERT_HAS_EXPIRED:
+		case X509_V_ERR_CRL_NOT_YET_VALID:
+		case X509_V_ERR_CRL_HAS_EXPIRED:
+			/* The peer certificate or a CA certificate has
+			 * expired. */
+			if (!edd->accept_expired_cert)
+				return (0);
+			break;
+		case X509_V_ERR_CERT_REVOKED:
+			/* The peer certificate or a CA certificate was
+			 * revoked. */
+			if (!edd->accept_revoked_cert)
+				return (0);
+			break;
+		case X509_V_ERR_UNABLE_TO_DECRYPT_CERT_SIGNATURE:
+		case X509_V_ERR_UNABLE_TO_DECRYPT_CRL_SIGNATURE:
+		case X509_V_ERR_UNABLE_TO_DECODE_ISSUER_PUBLIC_KEY:
+		case X509_V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD:
+		case X509_V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD:
+		case X509_V_ERR_ERROR_IN_CRL_LAST_UPDATE_FIELD:
+		case X509_V_ERR_ERROR_IN_CRL_NEXT_UPDATE_FIELD:
+			/* The verification couldn't be made because of
+			 * bad data. */
+			if (!edd->accept_corrupted_cert)
+				return (0);
+			break;
+		default:
+			/* Other types of error are not accepted. */
+			return (0);
+		}
+	}
+
+	/* Basic verification was successful. We now proceed with a more
+	 * XMPP-oriented verification, only on the peer certificate and
+	 * only if the caller specified an expected ID. */
+
+	/* XXX We need an option to accept non-matching certificates. */
+
+	/* Peer certificate is at depth 0. */
+	depth = X509_STORE_CTX_get_error_depth(x509_ctx);
+	if (depth > 0 || edd->expected_id == NULL)
+		return (1);
+
+	/* Get the peer certificate. */
+	cert = X509_STORE_CTX_get_current_cert(x509_ctx);
+
+	/* Get 'subjectAltName' extensions. */
+	gens = X509_get_ext_d2i(cert, NID_subject_alt_name, 0, 0);
+	if (gens) {
+		gens_count = sk_GENERAL_NAME_num(gens);
+		dnsname = NULL;
+
+		/* XXX We must first check for the 'xmpp' extension. */
+
+		/* We walk the extensions list to find the 'dNSName'
+		 * extension. */
+		for (i = 0; i < gens_count; ++i) {
+			gen = sk_GENERAL_NAME_value(gens, i);
+			if (gen->type != GEN_DNS)
+				/* This one is not a 'dNSName' extension. */
+				continue;
+
+			/* We expect the OpenSSL library to construct
+			 * GEN_DNS extesion objects as ASN1_IA5STRING
+			 * values. Check we got the right union member. */
+			if (ASN1_STRING_type(gen->d.ia5) != V_ASN1_IA5STRING)
+				continue;
+
+			dnsname = (char *)ASN1_STRING_data(gen->d.ia5);
+
+			/* ASN1_IA5STRING may contain NUL character; check
+			 * it. */
+			if (ASN1_STRING_length(gen->d.ia5) != strlen(dnsname))
+				continue;
+
+			/* Safe to treat as an ASCII string possibly
+			 * holding a DNS name */
+			if (match_hostname(dnsname, edd->expected_id)) {
+				sk_GENERAL_NAME_pop_free(gens,
+				    GENERAL_NAME_free);
+				return (1);
+			}
+		}
+
+		if (dnsname != NULL) {
+			/* A 'dNSName' extension was found but didn't
+			 * match. The certificate is not acceptable. */
+			/* XXX We must set the relevant X.509 error. */
+			sk_GENERAL_NAME_pop_free(gens, GENERAL_NAME_free);
+			return (0);
+		}
+
+		sk_GENERAL_NAME_pop_free(gens, GENERAL_NAME_free);
+	}
+
+	/* XXX We now fallback on the commonName extension. */
+
+	return (0);
 }
 
 /* -------------------------------------------------------------------
@@ -517,6 +723,16 @@ DRIVER_INIT(DRIVER_NAME)
 	/* Initialize OpenSSL. */
 	SSL_library_init();
 	SSL_load_error_strings();
+
+	/* This index is used to store application-specific data inside
+	 * SSL structs. This index is the same for each SSL instance,
+	 * that's why we keep it global.
+	 *
+	 * See:
+	 * http://www.mail-archive.com/openssl-users@openssl.org/msg52326.html
+	 */
+	ssl_ex_index = SSL_get_ex_new_index(0, "exmpp_tls_openssl_data",
+	    NULL, NULL, NULL);
 
 	return &tls_openssl_driver_entry;
 }
