809e743c90d24214b761b26e2ab0ff820335562b
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-06-24 08:46:39 +0000

    Add modules to read and create IQ for privacy list management.
    SVN Revision: 470

---
+++ src/client/exmpp_client_privacy.erl
@@ -0,0 +1,29 @@
+% $Id$
+
+%% @author Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
+
+%% @doc
+%% The module <strong>{@module}</strong> implements the initiating entity
+%% side of privacy lists management.
+
+-module(exmpp_client_privacy).
+-vsn('$Revision$').
+
+-include("exmpp.hrl").
+
+% Creating stanza.
+-export([
+  ack_list_push/1
+]).
+
+% --------------------------------------------------------------------
+% Creating stanza.
+% --------------------------------------------------------------------
+
+%% @spec (Push_IQ) -> Ack_IQ
+%%     Push_IQ = exmpp_xml:xmlel()
+%%     Ack_IQ = exmpp_xml:xmlel()
+%% @doc Make an `<iq/>' result to acknowledge the push.
+
+ack_list_push(Push_IQ) ->
+    exmpp_iq:result(Push_IQ).
---
+++ src/server/exmpp_server_privacy.erl
@@ -0,0 +1,43 @@
+% $Id$
+
+%% @author Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
+
+%% @doc
+%% The module <strong>{@module}</strong> implements the receiving entity
+%% side of privacy lists management.
+
+-module(exmpp_server_privacy).
+-vsn('$Revision$').
+
+-include("exmpp.hrl").
+
+% Creating stanza.
+-export([
+  list_push/2
+]).
+
+% --------------------------------------------------------------------
+% Creating stanza.
+% --------------------------------------------------------------------
+
+%% @spec (To, List_Name) -> Push_IQ
+%%     To = exmpp_jid:jid() | string()
+%%     List_Name = string()
+%%     Push_IQ = exmpp_xml:xmlel()
+%% @doc Create an `<iq/>' to notify `To' that the privacy list
+%% `List_Name' has been created or has changed.
+
+list_push(To, List_Name) ->
+    List0 = #xmlel{
+      ns = ?NS_JABBER_PRIVACY,
+      name = 'list'
+    },
+    List = exmpp_xml:set_attribute(List0, 'name', List_Name),
+    Query = #xmlel{
+      ns = ?NS_JABBER_PRIVACY,
+      name = 'query',
+      children = [List]
+    },
+    IQ = exmpp_iq:set(?NS_JABBER_CLIENT, Query,
+      exmpp_internals:random_id("push")),
+    exmpp_stanza:set_recipient(IQ, To).
