55e3a973797f57fb4496c78c475289fbf4e4ce54
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-07-09 12:56:54 +0000

    o  In to_list/2 and to_list/3, add a note about equivalent namespaces in exmpp_xml:xmlel_to_xmlelemen/3. o  In to_list/2, add Server Dialback to default prefixed namespace. o  Add a function to_list/1 which calls to_list/2 with common XMPP default namespaces.
    SVN Revision: 539

--- src/core/exmpp_stanza.erl
+++ src/core/exmpp_stanza.erl
@@ -71,7 +71,8 @@
 % Serialization wrappers.
 -export([
   to_list/2,
-  to_list/3
+  to_list/3,
+  to_list/1
 ]).
 
 % --------------------------------------------------------------------
@@ -684,25 +685,47 @@ get_text_in_error(Error) ->
 
 %% @spec (El, Default_NS) -> XML_Text
 %%     El = exmpp_xml:xmlel()
-%%     Default_NS = NS
+%%     Default_NS = NS | Equivalent_NSs
 %%     NS = atom() | string()
+%%     Equivalent_NSs = [NS]
 %%     XML_Text = string()
 %% @doc Serialize a stanza using the given default namespace.
 %%
-%% The XMPP namespace `http://etherx.jabber.org/streams' is included as
-%% a prefixd namespace, with the `stream' prefix.
+%% The XMPP namespace `http://etherx.jabber.org/streams' and the
+%% Server Dialback `jabber:server:dialback' are included as a prefixed
+%% namespace, with the `stream' prefix.
 
 to_list(El, Default_NS) ->
-    to_list(El, Default_NS, [{?NS_XMPP, ?NS_XMPP_pfx}]).
+    to_list(El, Default_NS,
+      [{?NS_XMPP, ?NS_XMPP_pfx}, {?NS_DIALBACK, ?NS_DIALBACK_pfx}]).
 
 %% @spec (El, Default_NS, Prefix) -> XML_Text
 %%     El = exmpp_xml:xmlel()
-%%     Default_NS = NS
+%%     Default_NS = NS | [NS]
 %%     Prefixed_NS = [{NS, Prefix}]
 %%     NS = atom() | string()
 %%     Prefix = string()
 %%     XML_Text = string()
 %% @doc Serialize a stanza using the given namespaces.
+%%
+%% To understand `Default_NS', see {@link exmpp_xml:xmlel_to_xmlelement/3}.
 
 to_list(El, Default_NS, Prefixed_NS) ->
     exmpp_xml:node_to_list(El, [Default_NS], Prefixed_NS).
+
+%% @spec (El) -> XML_Text
+%%     El = exmpp_xml:xmlel()
+%%     XML_Text = string()
+%% @doc Serialize a stanza using common XMPP default namespaces.
+%%
+%% This function calls {@link to_list/2} with `Default_NS' set to
+%% `[?NS_JABBER_CLIENT, ?NS_JABBER_SERVER, ?NS_COMPONENT_ACCEPT,
+%% ?NS_COMPONENT_CONNECT]'.
+
+to_list(El) ->
+    to_list(El, [
+        ?NS_JABBER_CLIENT,
+        ?NS_JABBER_SERVER,
+        ?NS_COMPONENT_ACCEPT,
+        ?NS_COMPONENT_CONNECT
+      ]).
