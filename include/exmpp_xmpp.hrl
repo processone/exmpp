dabae4385b780185f0022958059780c20fc74fdb
Jean-Sébastien Pédron <js.pedron@meetic-corp.com>
2008-08-05 15:37:52 +0000

    Improve guard macros. Now they can be used in 'if' statements as well.
    SVN Revision: 596

--- include/exmpp_xmpp.hrl
+++ include/exmpp_xmpp.hrl
@@ -19,28 +19,25 @@
 % --------------------------------------------------------------------
 
 % Guard expression to test a stanza as defined by XMPP-IM.
--define(IS_IM_STANZA(Stanza),
-  Stanza#xmlel.ns == ?NS_JABBER_CLIENT;
-  Stanza#xmlel.ns == ?NS_JABBER_SERVER
-).
+-define(IS_IM_STANZA(El), (
+  El#xmlel.ns == ?NS_JABBER_CLIENT orelse
+  El#xmlel.ns == ?NS_JABBER_SERVER
+)).
 
 % Guard expression to test a message.
--define(IS_MESSAGE(Message),
-  Message#xmlel.name == 'message', Message#xmlel.ns == ?NS_JABBER_CLIENT;
-  Message#xmlel.name == 'message', Message#xmlel.ns == ?NS_JABBER_SERVER
-).
+-define(IS_MESSAGE(El), (
+  ?IS_IM_STANZA(El) andalso El#xmlel.name == 'message'
+)).
 
 % Guard expression to test a presence.
--define(IS_PRESENCE(Presence),
-  Presence#xmlel.name == 'presence', Presence#xmlel.ns == ?NS_JABBER_CLIENT;
-  Presence#xmlel.name == 'presence', Presence#xmlel.ns == ?NS_JABBER_SERVER
-).
+-define(IS_PRESENCE(El), (
+  ?IS_IM_STANZA(El) andalso El#xmlel.name == 'presence'
+)).
 
 % Guard expression to test an IQ.
--define(IS_IQ(IQ),
-  IQ#xmlel.name == 'iq', IQ#xmlel.ns == ?NS_JABBER_CLIENT;
-  IQ#xmlel.name == 'iq', IQ#xmlel.ns == ?NS_JABBER_SERVER
-).
+-define(IS_IQ(El), (
+  ?IS_IM_STANZA(El) andalso El#xmlel.name == 'iq'
+)).
 
 % Guard expression to test a JID.
 -define(IS_JID(Jid),
