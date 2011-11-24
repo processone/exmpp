8f0e0801656894ca422f93d996e9c0dd93de10c9
Pablo Polvorin <pablo.polvorin@process-one.net>
2011-11-24 13:36:28 -0300

    Adapt exmpp_server_sasl

--- src/server/exmpp_server_sasl.erl
+++ src/server/exmpp_server_sasl.erl
@@ -47,8 +47,8 @@
 %% --------------------------------------------------------------------
 
 %% @spec (Mechanisms) -> Feature
-%%     Mechanisms = [binary() | string()]
-%%     Feature = exmpp_xml:xmlel()
+%%     Mechanisms = [binary() ]
+%%     Feature = exml:xmlel()
 %% @throws {sasl, feature_announcement, invalid_mechanisms_list, []} |
 %%         {sasl, feature_announcement, invalid_mechanism, Mechanism}
 %% @doc Make a feature announcement child.
@@ -56,31 +56,12 @@
 %% The result should then be passed to {@link exmpp_stream:features/1}.
 
 feature(Mechanisms) ->
-    Children = mechanisms_list(Mechanisms),
-    #xmlel{ns = ?NS_SASL,
-	   name = 'mechanisms',
-	   children = Children
-	  }.
+    {xmlel, <<"mechanisms">>, [{<<"xmlns">>, ?NS_SASL}], mechanisms_list(Mechanisms)}.
 
 mechanisms_list([]) ->
     throw({sasl, feature_announcement, invalid_mechanisms_list, []});
 mechanisms_list(Mechanisms) ->
-    mechanisms_list2(Mechanisms, []).
-
-mechanisms_list2([Mechanism | Rest], Children) ->
-    case io_lib:deep_char_list(Mechanism) of
-        true ->
-            Child = #xmlel{ns = ?NS_SASL,
-			   name = 'mechanism'
-			  },
-            mechanisms_list2(Rest,
-			     [exmpp_xml:set_cdata(Child, Mechanism)
-			      | Children]);
-        false ->
-            throw({sasl, feature_announcement, invalid_mechanism, Mechanism})
-    end;
-mechanisms_list2([], Children) ->
-    lists:reverse(Children).
+	[{xmlel, <<"mechanism">>, [], [{cdata, Mechanism}]} || Mechanism <- Mechanisms].
 
 %% --------------------------------------------------------------------
 %% SASL exchange.
@@ -88,73 +69,61 @@ mechanisms_list2([], Children) ->
 
 standard_conditions() ->
     [
-     {'aborted'},
-     {'incorrect-encoding'},
-     {'invalid-authzid'},
-     {'invalid-mechanism'},
-     {'mechanism-too-weak'},
-     {'not-authorized'},
-     {'temporary-auth-failure'},
+     {<<"aborted">>},
+     {<<"incorrect-encoding">>},
+     {<<"invalid-authzid">>},
+     {<<"invalid-mechanism">>},
+     {<<"mechanism-too-weak">>},
+     {<<"not-authorized">>},
+     {<<"temporary-auth-failure">>},
      %% rfc3920bis
-     {'account-disabled'},
-     {'credentials-expired'},
-     {'encryption-required'},
-     {'malformed-request'},
-     {'undefined-condition'}
+     {<<"account-disabled">>},
+     {<<"credentials-expired">>},
+     {<<"encryption-required">>},
+     {<<"malformed-request">>},
+     {<<"undefined-condition">>}
     ].
 
 %% @spec (Challenge) -> Challenge_El
-%%     Challenge = string() | none
-%%     Challenge_El = exmpp_xml:xmlel()
+%%     Challenge = binary() | none
+%%     Challenge_El = exml:xmlel()
 %% @doc Prepare a `<challenge/>' element with the given challenge.
 %%
 %% `Challenge' will be Base64-encoded by this function.
 
 challenge(none) ->
-    #xmlel{ns = ?NS_SASL,
-	   name = 'challenge'
-	  };
+	{xmlel, <<"challenge">>, [{<<"xmlns">>, ?NS_SASL}], []};
 challenge(Challenge) ->
-    El = #xmlel{ns = ?NS_SASL,
-		name = 'challenge'
-	       },
-    exmpp_xml:set_cdata(El, base64:encode_to_string(Challenge)).
+	{xmlel, <<"challenge">>, [{<<"xmlns">>, ?NS_SASL}], [{cdata, base64:encode(Challenge)}]}.
 
 %% @spec () -> Success_El
-%%     Success_El = exmpp_xml:xmlel()
+%%     Success_El = exml:xmlel()
 %% @doc Prepare a `<success/>' element.
 
 success() ->
     success(none).
 
 %% @spec (Data) -> Success_El
-%%     Data = string() | none
-%%     Success_El = exmpp_xml:xmlel()
+%%     Data = binary() | none
+%%     Success_El = exml:xmlel()
 %% @doc Prepare a `<success/>' element with supplied XML character data.
 %% `Data' will be Base64-encoded by this function.
 
 success(none) ->
-    #xmlel{ns = ?NS_SASL,
-	   name = 'success'
-	  };
+	{xmlel, <<"success">>, [{<<"xmlns">>, ?NS_SASL}], []};
 success(Data) ->
-    El = #xmlel{ns = ?NS_SASL,
-		name = 'success'
-	       },
-    exmpp_xml:set_cdata(El, base64:encode_to_string(Data)).
+	{xmlel, <<"success">>, [{<<"xmlns">>, ?NS_SASL}], [{cdata, base64:encode(Data)}]}.
 
 %% @spec () -> Failure
-%%     Failure = exmpp_xml:xmlel()
+%%     Failure = exml:xmlel()
 %% @doc Prepare a `<failure/>' element.
 
 failure() ->
-    #xmlel{ns = ?NS_SASL,
-	   name = 'failure'
-	  }.
+	{xmlel, <<"failure">>, [{<<"xmlns">>, ?NS_SASL}],[]}.
 
 %% @spec (Condition) -> Failure
-%%     Condition = atom()
-%%     Failure = exmpp_xml:xmlel()
+%%     Condition = binary()
+%%     Failure = exml:xmlel()
 %% @doc Prepare a `<failure/>' element with a defined condition.
 
 failure(Condition) ->
@@ -162,56 +131,42 @@ failure(Condition) ->
         true  -> ok;
         false -> throw({sasl, failure, invalid_condition, Condition})
     end,
-    Condition_El = #xmlel{ns = ?NS_SASL,
-			  name = Condition
-			 },
-    exmpp_xml:append_child(failure(), Condition_El).
+    exml:append_child(failure(), {xmlel,Condition, [], []}).
 
 %% @spec (Condition, Text) -> Failure
-%%     Condition = atom()
-%%     Text = string()
-%%     Failure = exmpp_xml:xmlel()
+%%     Condition = binary()
+%%     Text = binary()
+%%     Failure = exml:xmlel()
 %% @doc Prepare a `<failure/>' element with a defined condition and text.
 
-failure(Condition, "") ->
+failure(Condition, <<>>) ->
     failure(Condition);
 failure(Condition, Text) ->
-    case lists:keymember(Condition, 1, standard_conditions()) of
-        true  -> ok;
-        false -> throw({sasl, failure, invalid_condition, Condition})
-    end,
-    Condition_El = #xmlel{ns = ?NS_SASL,
-			  name = Condition
-			 },
-    Text_El = #xmlel{ns = ?NS_SASL,
-			  name = text,
-			  children = exmpp_xml:cdata(Text)
-			 },
-    exmpp_xml:append_children(failure(), [Condition_El, Text_El]).
+	exml:append_child(failure(Condition), {xmlel, <<"text">>, [], [{cdata, Text}]}).
 
 %% @spec (El) -> Type
-%%     El = exmpp_xml:xmlel()
+%%     El = exml:xmlel()
 %%     Type = Auth | Response | Abort
-%%     Auth = {auth, Mechanism, none | string()}
-%%     Mechanism = string()
-%%     Response = {response, string()}
+%%     Auth = {auth, Mechanism, none | binary()}
+%%     Mechanism = binary()
+%%     Response = {response, binary()}
 %%     Abort = abort
 %% @throws {sasl, next_step, unexpected_element, El}
 %% @doc Extract the response that the initiating entity sent.
 %%
 %% Any response data is Base64-decoded.
 
-next_step(#xmlel{ns = ?NS_SASL, name = 'auth'} = El) ->
-    Mechanism = exmpp_xml:get_attribute_as_list(El, <<"mechanism">>, undefined),
-    case exmpp_utils:strip(exmpp_xml:get_cdata_as_list(El)) of
-        ""      -> {auth, Mechanism, none};
-        "="     -> {auth, Mechanism, ""};
-        Encoded -> {auth, Mechanism, base64:decode_to_string(Encoded)}
+next_step({xmlel, <<"auth">>, _, _} = El) ->
+    Mechanism = exml:get_attribute(El, <<"mechanism">>),
+    case exmpp_utils:strip(exml:get_cdata(El)) of
+        <<>>      -> {auth, Mechanism, none};
+        <<"=">>     -> {auth, Mechanism, <<>>};
+        Encoded -> {auth, Mechanism, base64:decode(Encoded)}
     end;
-next_step(#xmlel{ns = ?NS_SASL, name = 'response'} = El) ->
-    Encoded = exmpp_xml:get_cdata_as_list(El),
-    {response, base64:decode_to_string(Encoded)};
-next_step(#xmlel{ns = ?NS_SASL, name = 'abort'}) ->
+next_step({xmlel, <<"response">>, _, _} = El) ->
+    Encoded = exml:get_cdata(El),
+    {response, base64:decode(Encoded)};
+next_step({xmlel, <<"abort">>, _, _}) ->
     abort;
 next_step(El) ->
     throw({sasl, next_step, unexpected_element, El}).
---
+++ test/exmpp_server_sasl_test.erl
@@ -0,0 +1,70 @@
+-module(exmpp_server_sasl_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("exmpp.hrl").
+
+
+feature_test() ->
+	F = exmpp_server_sasl:feature([<<"EXTERNAL">>, <<"PLAIN">>]),
+	?assertMatch({xmlel, <<"mechanisms">>, _, _}, F),
+	?assertEqual(<<"urn:ietf:params:xml:ns:xmpp-sasl">>, exml:get_attribute(F, <<"xmlns">>)),
+	?assertEqual([<<"EXTERNAL">>, <<"PLAIN">>],
+		[ exml:get_cdata(M) ||{xmlel, <<"mechanism">>, _, _} =M <- exml:get_elements(F)]),
+	ok.
+
+
+challenge_test() ->
+	?assertMatch({xmlel, <<"challenge">>, _, []} , exmpp_server_sasl:challenge(none)),
+	C = exmpp_server_sasl:challenge(<<"aaa">>),
+	?assertEqual(<<"urn:ietf:params:xml:ns:xmpp-sasl">>, exml:get_attribute(C, <<"xmlns">>)),
+	?assertEqual(base64:encode(<<"aaa">>), exml:get_cdata(C)),
+	ok.
+
+success_test() ->
+	?assertMatch({xmlel, <<"success">>, _, []} , exmpp_server_sasl:success(none)),
+	C = exmpp_server_sasl:success(<<"aaa">>),
+	?assertEqual(<<"urn:ietf:params:xml:ns:xmpp-sasl">>, exml:get_attribute(C, <<"xmlns">>)),
+	?assertEqual(base64:encode(<<"aaa">>), exml:get_cdata(C)),
+	ok.
+
+failure_test() ->
+	?assertMatch({xmlel, <<"failure">>, _, _}, exmpp_server_sasl:failure()),
+	?assertEqual(<<"urn:ietf:params:xml:ns:xmpp-sasl">>, 
+		exml:get_attribute(exmpp_server_sasl:failure(), <<"xmlns">>)),
+	?assertMatch({xmlel, <<"failure">>, _, _}, exmpp_server_sasl:failure(<<"not-authorized">>)),
+	?assertEqual(<<"urn:ietf:params:xml:ns:xmpp-sasl">>, 
+		exml:get_attribute(exmpp_server_sasl:failure(<<"not-authorized">>), <<"xmlns">>)),
+	?assertMatch([{xmlel, <<"not-authorized">>, [], []}],
+		exml:get_elements(exmpp_server_sasl:failure(<<"not-authorized">>))),
+
+	?assertMatch(<<"hey">>,
+		exml:get_path(exmpp_server_sasl:failure(<<"not-authorized">>, <<"hey">>), [{element, <<"text">>},cdata])),
+	ok.
+
+next_step1_test() ->
+	S = <<"<auth xmlns='urn:ietf:params:xml:ns:xmpp-sasl'
+         mechanism='SCRAM-SHA-1'>
+     biwsbj1qdWxpZXQscj1vTXNUQUF3QUFBQU1BQUFBTlAwVEFBQUFBQUJQVTBBQQ==
+   </auth>">>,
+	{ok, [R]} = exml:parse_document(S),
+	?assertEqual({auth, <<"SCRAM-SHA-1">>, <<"n,,n=juliet,r=oMsTAAwAAAAMAAAANP0TAAAAAABPU0AA">>}, exmpp_server_sasl:next_step(R)),
+	ok.
+
+next_step2_test() ->
+	S = <<"<response xmlns='urn:ietf:params:xml:ns:xmpp-sasl'>
+     Yz1iaXdzLHI9b01zVEFBd0FBQUFNQUFBQU5QMFRBQUFBQUFCUFUwQUFlMTI0N
+     jk1Yi02OWE5LTRkZTYtOWMzMC1iNTFiMzgwOGM1OWUscD1VQTU3dE0vU3ZwQV
+     RCa0gyRlhzMFdEWHZKWXc9
+   </response>">>,
+	{ok, [R]} = exml:parse_document(S),
+	?assertEqual({response, <<"c=biws,r=oMsTAAwAAAAMAAAANP0TAAAAAABPU0AAe124695b-69a9-4de6-9c30-b51b3808c59e,p=UA57tM/SvpATBkH2FXs0WDXvJYw=">>}, 
+		exmpp_server_sasl:next_step(R)),
+
+	ok.
+
+next_step3_test() ->
+	R = {xmlel, <<"abort">>, [{<<"xmlns">>,<<"urn:ietf:params:xml:ns:xmpp-sasl">>}], []},
+	?assertEqual(abort, exmpp_server_sasl:next_step(R)),
+	ok.
+
