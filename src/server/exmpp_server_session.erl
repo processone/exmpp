4175e2b67e284dfad9a87edbe7a95db06aab3fd2
Pablo Polvorin <pablo.polvorin@process-one.net>
2011-11-24 16:09:15 -0300

    Adapt exmpp_server_session

--- src/server/exmpp_server_session.erl
+++ src/server/exmpp_server_session.erl
@@ -38,30 +38,28 @@
 %% --------------------------------------------------------------------
 
 %% @spec () -> Feature
-%%     Feature = exmpp_xml:xmlel()
+%%     Feature = exml:xmlel()
 %% @doc Make a feature annoucement child.
 %%
 %% The result should then be passed to {@link exmpp_stream:features/1}.
 
 feature() ->
-    #xmlel{ns = ?NS_SESSION,
-	   name = 'session'
-	  }.
+	{xmlel, <<"session">>, [{<<"xmlns">>, ?NS_SESSION}], []}.
 
 %% --------------------------------------------------------------------
 %% Session establishment.
 %% --------------------------------------------------------------------
 
-%% @spec (IQ) -> bool()
-%%     IQ = exmpp_xml:xmlel()
+%% @spec (IQ) -> boolean()
+%%     IQ = exml:xmlel()
 %% @doc Tell if the initiating entity wants to establish a session.
 
 want_establishment(IQ) when ?IS_IQ(IQ) ->
     case exmpp_iq:get_type(IQ) of
-        'set' ->
-            case exmpp_iq:get_request(IQ) of
-                #xmlel{ns = ?NS_SESSION, name = 'session'} -> true;
-                _                                          -> false
+        <<"set">> ->
+            case exmpp_iq:get_payload_ns(IQ) of
+                ?NS_SESSION -> true;
+                _           -> false
             end;
         _ ->
             false
@@ -70,8 +68,8 @@ want_establishment(_Stanza) ->
     false.
 
 %% @spec (IQ) -> Result_IQ
-%%     IQ = exmpp_xml:xmlel()
-%%     Result_IQ = exmpp_xml:xmlel()
+%%     IQ = exml:xmlel()
+%%     Result_IQ = exml:xmlel()
 %% @doc Prepare a result IQ to inform the initiating entity that the
 %% session is created.
 
@@ -79,11 +77,11 @@ establish(IQ) when ?IS_IQ(IQ) ->
     exmpp_iq:result(IQ).
 
 %% @spec (IQ, Condition) -> Error_IQ
-%%     IQ = exmpp_xml:xmlel()
-%%     Condition = atom()
-%%     Error_IQ = exmpp_xml:xmlel()
+%%     IQ = exml:xmlel()
+%%     Condition = binary()
+%%     Error_IQ = exml:xmlel()
 %% @doc Prepare an error reply to `IQ'.
 
 error(IQ, Condition) when ?IS_IQ(IQ) ->
-    Error = exmpp_stanza:error(IQ#xmlel.ns, Condition),
+    Error = exmpp_stanza:error(Condition),
     exmpp_iq:error(IQ, Error).
---
+++ test/exmpp_server_session_test.erl
@@ -0,0 +1,22 @@
+-module(exmpp_server_session_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("exmpp.hrl").
+
+
+feature_test() ->
+	B = exmpp_server_session:feature(),
+	?assertMatch({xmlel, <<"session">>, _, _}, B),
+	?assertEqual(<<"urn:ietf:params:xml:ns:xmpp-session">>, exml:get_attribute(B, <<"xmlns">>)),
+	ok.
+
+want_establishment_test() ->
+	S = <<"<iq type='set' id='1'><session xmlns='urn:ietf:params:xml:ns:xmpp-session'/></iq>">>,
+	{ok, [R]} = exml:parse_document(S),
+	?assert(exmpp_server_session:want_establishment(R)),
+	?assertNot(exmpp_server_session:want_establishment({xmlel, <<"iq">>, [{<<"type">>, <<"get">>}], []})),
+	?assertNot(exmpp_server_session:want_establishment({xmlel, <<"iq">>, [{<<"type">>, <<"set">>}], [{xmlel, <<"query">>, [], []}]})),
+	ok.
+
+
